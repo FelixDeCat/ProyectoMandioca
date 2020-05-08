919238ff5afaa34ae616913f4cb38362e45fd726
SantiMinio <santtiago.mino@davinci.edu.ar>
2020-05-07 21:41:40 -0300

    Generic Movement Component Enemy (Todavia no implementado)
    Como varios personajes van a compartir maneras de moverse, para no almacenar los datos de movimiento en todos los estados que los necesiten, hice este componente como una manera de ahorrar algo de memoria.

---
+++ Assets/Game/Scripts/Enemys/GenericEnemyMove.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericEnemyMove : MonoBehaviour
+{
+    float currentSpeed;
+    [SerializeField] float initialSpeed = 5;
+    Rigidbody rb;
+    Transform root;
+    [SerializeField] float avoidWeight = 1.5f;
+    [SerializeField] float avoidanceRadious = 2;
+    [SerializeField] float rootSpeed = 2;
+    [SerializeField] LayerMask avoidMask = 0;
+
+    public float GetCurrentSpeed() => currentSpeed;
+
+    public void SetCurrentSpeed(float _speed) => currentSpeed = _speed;
+
+    public float ModifySpeed(float _speed) => currentSpeed += _speed;
+
+    public void SetDefaultSpeed() => currentSpeed = initialSpeed;
+
+
+    ///<summary> Movement with Rigidbody. Returns a promedy with the values of the vector result (for animations or rotation).
+    ///</summary>
+    public float MoveWRigidbodyF(Vector3 dir)
+    {
+        float y = rb.velocity.y;
+
+        rb.velocity = new Vector3(dir.x * currentSpeed, y, dir.z * currentSpeed);
+
+        return dir.normalized.x + dir.normalized.z;
+    }
+
+    ///<summary> Movement with Rigidbody. Returns the vector result normalized (for animations or rotation).
+    ///</summary>
+    public Vector3 MoveWRigidbodyV(Vector3 dir)
+    {
+        float y = rb.velocity.y;
+
+        rb.velocity = new Vector3(dir.x * currentSpeed, y, dir.z * currentSpeed);
+
+        return dir.normalized;
+    }
+
+    ///<summary> Rotation with Transform with lerp (forward/dir value).
+    ///</summary>
+    public void Rotation(Vector3 forward)
+    {
+        root.forward = Vector3.Lerp(root.forward, forward, rootSpeed * Time.deltaTime);
+    }
+
+    ///<summary> Stop the velocity of the Rigidbody.
+    ///</summary>
+    public void StopMove()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    ///<summary> Movement with transform. Returns a promedy with the values of the vector result (for animations or rotation).
+    ///</summary>
+    public float MoveWTransformF(Vector3 dir)
+    {
+        root.position += dir * Time.deltaTime * currentSpeed;
+
+        return dir.normalized.x + dir.normalized.z;
+    }
+
+    ///<summary> Movement with Rigidbody. Returns the vector result normalized (for animations or rotation).
+    ///</summary>
+    public Vector3 MoveWTransformV(Vector3 dir)
+    {
+        root.position += dir * Time.deltaTime * currentSpeed;
+
+        return dir.normalized;
+    }
+
+
+    Transform obs;
+    ///<summary> Basic Obstacle Avoidance. Returns a new vector result.
+    ///</summary>
+    public Vector3 ObstacleAvoidance(Vector3 dir)
+    {
+        obs = null;
+        var friends = Physics.OverlapSphere(root.position, avoidanceRadious, avoidMask);
+        if (friends.Length > 0)
+        {
+            foreach (var item in friends)
+            {
+                if (item != this)
+                {
+                    if (!obs)
+                        obs = item.transform;
+                    else if (Vector3.Distance(item.transform.position, root.position) < Vector3.Distance(obs.position, root.position))
+                        obs = item.transform;
+                }
+            }
+        }
+
+        if (obs)
+        {
+            Vector3 diraux = (root.position - obs.position).normalized;
+
+            diraux = new Vector3(diraux.x, 0, diraux.z);
+
+            dir += diraux * avoidWeight;
+        }
+
+        return dir;
+    }
+}
