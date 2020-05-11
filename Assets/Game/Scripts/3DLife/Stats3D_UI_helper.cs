8e5b7beb46c021faaf27667d13cf8188a1a12cca
Francisco Criscuolo <>
2020-05-11 19:26:05 -0300

    Integracion un poco cabeza de UI3d

---
+++ Assets/Game/Scripts/3DLife/Stats3D_UI_helper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats3D_UI_helper : MonoBehaviour
+{
+    public FrontendStatBase lifeBar;
+    public FrontendStatBase expBar;
+}
