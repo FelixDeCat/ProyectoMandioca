68b1e072ba97ea7d901c5d921664cfc5c2387cc5
Agezma <Agezma@live.com>
2020-05-11 22:09:33 -0300

    ShieldSword / FlashEnemies

--- Assets/Game/Scripts/PlayObjectCore/Entities/Entity_Walker/NPCs/EnemyBase.cs
+++ Assets/Game/Scripts/PlayObjectCore/Entities/Entity_Walker/NPCs/EnemyBase.cs
@@ -117,4 +117,29 @@ public abstract class EnemyBase : NPCBase, ICombatDirector
         AddEffectTick(MyUpdate);
     }
     #endregion
+
+   
+    public IEnumerator OnHitted(Material[] myMat, float onHitFlashTime, Color onHitColor)
+    {
+        var smr = GetComponentInChildren<SkinnedMeshRenderer>();
+        if (smr != null)
+        {
+            myMat = smr.materials;
+
+            Material[] mats = smr.materials;
+            smr.materials = mats;
+            for (int i = 0; i < onHitFlashTime; i++)
+            {
+                if (i < (onHitFlashTime / 2f))
+                {
+                    mats[1].SetColor("_EmissionColor", Color.Lerp(Color.black, onHitColor, i / (onHitFlashTime / 2f)));
+                }
+                else
+                {
+                    mats[1].SetColor("_EmissionColor", Color.Lerp(onHitColor, Color.black, (i - (onHitFlashTime / 2f)) / (onHitFlashTime / 2f)));
+                }
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
 }
