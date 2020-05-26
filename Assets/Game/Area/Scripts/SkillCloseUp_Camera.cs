6ca221788f0ca05bb1de0001ca99b6cb41cafe2e
Francisco Criscuolo <>
2020-05-26 09:53:51 -0300

    skillCloseUp_camera

---
+++ Assets/Game/Area/Scripts/SkillCloseUp_Camera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCloseUp_Camera : MonoBehaviour
+{ 
+    Action OnTurnOn;
+    Action OnTurnOff;
+
+    public void TurnOnSkillCamera(){OnTurnOn?.Invoke();transform.gameObject.SetActive(true);}
+
+    public void TurnOffSkillCamera(){OnTurnOff?.Invoke();transform.gameObject.SetActive(true);} 
+
+    public void SubscribeOnTurnOnCamera(Action listener) => OnTurnOn += listener;
+    public void UnsubscribeOnTurnOnCamera(Action listener) => OnTurnOn -= listener;
+    
+    public void SubscribeOnTurnOffCamera(Action listener) => OnTurnOff += listener;
+    public void UnsubscribeOnTurnOffCamera(Action listener) => OnTurnOff -= listener;
+}
