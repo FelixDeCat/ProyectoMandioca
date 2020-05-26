eb43ecc886cf38bb82584ecfa3c4b680b968a53a
Francisco Criscuolo <>
2020-05-26 10:00:38 -0300

    fix skill camera

--- Assets/Game/Area/Scripts/SkillCloseUp_Camera.cs
+++ Assets/Game/Area/Scripts/SkillCloseUp_Camera.cs
@@ -10,7 +10,7 @@ public class SkillCloseUp_Camera : MonoBehaviour
 
     public void TurnOnSkillCamera(){OnTurnOn?.Invoke();transform.gameObject.SetActive(true);}
 
-    public void TurnOffSkillCamera(){OnTurnOff?.Invoke();transform.gameObject.SetActive(true);} 
+    public void TurnOffSkillCamera(){OnTurnOff?.Invoke();transform.gameObject.SetActive(false);} 
 
     public void SubscribeOnTurnOnCamera(Action listener) => OnTurnOn += listener;
     public void UnsubscribeOnTurnOnCamera(Action listener) => OnTurnOn -= listener;
