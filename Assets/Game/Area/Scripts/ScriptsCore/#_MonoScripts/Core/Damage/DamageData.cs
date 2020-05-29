de0d2a6f51290050722d5a2da6468656a47d2158
SantiMinio <santtiago.mino@davinci.edu.ar>
2020-05-28 21:13:44 -0300

    DamageData y DamageReceive (Falta implementar)

--- Assets/Game/Area/Scripts/ScriptsCore/#_MonoScripts/Core/Damage/DamageData.cs
+++ Assets/Game/Area/Scripts/ScriptsCore/#_MonoScripts/Core/Damage/DamageData.cs
@@ -4,5 +4,59 @@ using UnityEngine;
 
 public class DamageData : MonoBehaviour
 {
-    //int dmg, Vector3 attack_pos, Damagetype damagetype, EntityBase owner_entity
+    [HideInInspector] public int damage;
+    [HideInInspector] public int initDamage;
+    [HideInInspector] public int tickDamage;
+    [HideInInspector] public int finalDamage;
+
+    [HideInInspector] public Damagetype damageType;
+
+    [HideInInspector] public Vector3 owner_position;
+
+    [HideInInspector] public float knockbackForce;
+
+    [HideInInspector] public bool hasDamageTick;
+    [HideInInspector] public float damageTime;
+    [HideInInspector] public Vector3 attackDir;
+    [HideInInspector] public EntityBase owner;
+
+    public void Initialize(EntityBase _owner)
+    {
+        owner = _owner;
+    }
+
+    public DamageData SetDamage(int dmg)
+    {
+        damage = dmg;
+        return this;
+    }
+
+    public DamageData SetDamageType(Damagetype dmgType)
+    {
+        damageType = dmgType;
+        return this;
+    }
+
+    public DamageData SetDamageTick(bool hasTick, int initDmg = 0, int tickDmg = 0, int finalDmg = 0, float dmgTime = 0)
+    {
+        hasDamageTick = hasTick;
+        initDamage = initDmg;
+        tickDamage = tickDmg;
+        finalDamage = finalDmg;
+        damageTime = dmgTime;
+        return this;
+    }
+
+    public DamageData SetPositionAndDirection(Vector3 pos, Vector3 dir = default)
+    {
+        owner_position = pos;
+        attackDir = dir;
+        return this;
+    }
+
+    public DamageData SetKnockback(float _knockback)
+    {
+        knockbackForce = _knockback;
+        return this;
+    }
 }
---
+++ Assets/Game/Area/Scripts/ScriptsCore/#_MonoScripts/Core/Damage/DamageReceiver.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DamageReceiver : MonoBehaviour
+{
+    [SerializeField] List<Damagetype> invulnerability = new List<Damagetype>();
+    [SerializeField] List<Damagetype> resistances = new List<Damagetype>();
+    [SerializeField] List<Damagetype> debilities = new List<Damagetype>();
+
+
+    [SerializeField] int debilityAddDmg;
+    [SerializeField] int resistanceRestDmg;
+
+    bool blockEntity;
+    bool parryEntity;
+
+
+    Func<int, bool> OnHit;
+    Action<DamageData> takeDmg;
+    Action OnDead;
+    Action InmuneFeedback;
+    Func<bool> IsDmg;
+    Transform ownerRoot;
+
+    Func<Vector3, Vector3, Vector3, bool> IsBlock;
+    Func<Vector3, Vector3, Vector3, bool> IsParry;
+    Action Block;
+    Action Parry;
+
+    Rigidbody rb;
+
+    public void Initialize(Transform _ownerRoot, Func<bool> _IsDmg, Action _OnDead, Action<DamageData> _takeDmg,
+        Rigidbody _rb, Func<int, bool> _OnHit, Action _InmuneFeedback = default)
+    {
+        ownerRoot = _ownerRoot;
+        takeDmg = _takeDmg;
+        OnDead = _OnDead;
+        IsDmg = _IsDmg;
+        InmuneFeedback = _InmuneFeedback;
+        OnHit = _OnHit;
+        rb = _rb;
+    }
+    public DamageReceiver SetBlock(Func<Vector3, Vector3, Vector3, bool> _IsBlock, Action _Block)
+    {
+        blockEntity = true;
+        IsBlock = _IsBlock;
+        Block = _Block;
+        return this;
+    }
+
+    public DamageReceiver SetParry(Func<Vector3, Vector3, Vector3, bool> _IsParry, Action _Parry)
+    {
+        parryEntity = true;
+        IsParry = _IsParry;
+        Parry = _Parry;
+        return this;
+    }
+
+
+    public Attack_Result TakeDamage(DamageData data)
+    {
+        if (IsDmg()) return Attack_Result.inmune;
+
+        if (invulnerability.Contains(data.damageType))
+        {
+            InmuneFeedback();
+            return Attack_Result.inmune;
+        }
+
+        if (parryEntity)
+        {
+            if (IsParry(ownerRoot.position, data.owner_position, ownerRoot.forward))
+            {
+                Parry();
+                return Attack_Result.blocked;
+            }
+        }
+        if (blockEntity)
+        {
+            if (IsBlock(ownerRoot.position, data.owner_position, ownerRoot.forward))
+            {
+                Block();
+                return Attack_Result.blocked;
+            }
+        }
+
+        int dmg = data.damage;
+
+        if (resistances.Contains(data.damageType)) dmg -= resistanceRestDmg;
+        else if (debilities.Contains(data.damageType)) dmg += debilityAddDmg;
+
+        Vector3 aux = (ownerRoot.position - data.owner_position).normalized;
+        rb.AddForce(aux * data.knockbackForce + data.attackDir, ForceMode.Impulse);
+
+        bool death = OnHit(dmg);
+        takeDmg(data);
+
+        return death ? Attack_Result.death : Attack_Result.sucessful;
+    }
+
+    public void AddInvulnerability(Damagetype inv) { if (!invulnerability.Contains(inv)) invulnerability.Add(inv); }
+
+    public void RemoveInvulnerability(Damagetype inv) { if (invulnerability.Contains(inv)) invulnerability.Remove(inv); }
+
+    public void AddResistance(Damagetype inv) { if (!resistances.Contains(inv)) resistances.Add(inv); }
+
+    public void RemoveResistance(Damagetype inv) { if (resistances.Contains(inv)) resistances.Remove(inv); }
+
+    public void AddDebility(Damagetype inv) { if (!debilities.Contains(inv)) debilities.Add(inv); }
+
+    public void RemoveDebility(Damagetype inv) { if (debilities.Contains(inv)) debilities.Remove(inv); }
+}
