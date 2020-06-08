ab312bc511c26fd00e9fabfc7dffe19edad6c33f
Francisco Criscuolo <>
2020-06-08 17:17:42 -0300

    base grid

--- Assets/Game/Area/Scripts/DevelopTools/SpatialGrid/GridEntity.cs
+++ Assets/Game/Area/Scripts/DevelopTools/SpatialGrid/GridEntity.cs
@@ -4,36 +4,6 @@ using UnityEngine;
 //[ExecuteInEditMode]
 public class GridEntity : MonoBehaviour
 {
-	public event Action<GridEntity> OnMove = delegate {};
 	//public Vector3 velocity = new Vector3(0, 0, 0);
     public bool onGrid;
-    //Renderer _rend;
-    private PlayObject _playObject;
-    private void Awake()
-    {
-        _playObject = GetComponent<PlayObject>();
-    }
-
-    void Update() {
-        
-//        if (onGrid)
-//        {
-//            Debug.Log(_playObject);
-//    
-//            if (_playObject != null)
-//            {
-//                Debug.Log(_playObject);
-//                _playObject.On();
-//            }
-//        }
-//        else
-//            _playObject.Off();
-        
-        
-        
-//            _rend.material.color = Color.gray;
-//		//Optimization: Only on *actual* move
-//		transform.position += velocity * Time.deltaTime;
-//	    OnMove(this);
-	}
 }
