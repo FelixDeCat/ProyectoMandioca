334b5b0e0d2f01219859969c52ae7ea6b5252e87
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-06-15 15:55:41 -0300

    para trabajar en paralelo

---
+++ Assets/EnableRender.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class EnableRender : MonoBehaviour
+{
+    [Header("EXECUTE IN EDIT MODE")]
+    public bool execute;
+    bool enable;
+    void Update()
+    {
+        if (execute)
+        {
+            execute = false;
+            enable = !enable;
+            GetComponentsInChildren<Renderer>().ToList().ForEach(x => x.enabled = enable);
+        }
+    }
+}
