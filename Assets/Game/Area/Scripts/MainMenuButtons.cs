89546d4c07170c84199c0c881d533c41135ee588
SantiMinio <santtiago.mino@davinci.edu.ar>
2020-06-21 20:55:03 -0300

    Menu manejable con joystick :video_game:

---
+++ Assets/Game/Area/Scripts/MainMenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    EventSystem eventSystem;
+    [SerializeField] GameObject primaryButton;
+
+    private void Awake()
+    {
+        eventSystem = FindObjectOfType<EventSystem>();
+        eventSystem?.SetSelectedGameObject(primaryButton);
+    }
+
+    public void SelectButton(GameObject button) => eventSystem?.SetSelectedGameObject(button);
+}
