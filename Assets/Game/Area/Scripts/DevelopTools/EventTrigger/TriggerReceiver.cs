6e243555cee66816018b841130e850a04383c219
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-06-22 18:33:42 -0300

    event dispacher por trigger

---
+++ Assets/Game/Area/Scripts/DevelopTools/EventTrigger/TriggerReceiver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public abstract class TriggerReceiver : MonoBehaviour
+{
+    [SerializeField] protected bool has_one_Shot = false;
+    bool oneshot;
+
+    public void Execute()
+    {
+        if (has_one_Shot)
+        {
+            if (!oneshot)
+            {
+                oneshot = true;
+                OnExecute();
+            }
+        }
+        else
+        {
+            OnExecute();
+        }
+    }
+
+    protected abstract void OnExecute();
+}
---
+++ Assets/Game/Area/Scripts/DevelopTools/EventTrigger/TriggerReceivers/DialogDispacher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogDispacher : TriggerReceiver
+{
+    private void Awake()
+    {
+        has_one_Shot = true;
+    }
+
+    protected override void OnExecute()
+    {
+        Debug.LogWarning("Empieza un dialogo");
+    }
+}
