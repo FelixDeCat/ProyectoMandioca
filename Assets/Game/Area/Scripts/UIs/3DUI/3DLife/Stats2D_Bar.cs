5cd517c5d278b543784a0f36ba742a294244433b
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-06-27 04:33:57 -0300

    wendigo end portal + corazones parte amarilla con anim

--- Assets/Game/Area/Scripts/UIs/3DUI/3DLife/Stats2D_Bar.cs
+++ Assets/Game/Area/Scripts/UIs/3DUI/3DLife/Stats2D_Bar.cs
@@ -8,40 +8,90 @@ public class Stats2D_Bar : FrontendStatBase
 {
     [SerializeField] Image img;
     [SerializeField] Image img_back;
-    private void Start()
-    {
-       // img = GetComponent<Image>();
-    }
 
-
-
+    bool anim;
+    public float speed = 0.5f;
+    float val_destiny;
+    public float delay = 0.5f;
 
     public override void OnValueChange(int value, int max = 100, bool anim = false)
     {
         float val = value;
         float mx = max;
         float numberF = (val / mx);
-        Debug.Log($"{numberF} y el de la fill amount es {img.fillAmount}" );
-        
+        val_destiny = numberF;
         img.fillAmount = numberF;
+        img_back.enabled = true;
+        if (img_back.fillAmount < numberF)
+        { 
+           img_back.fillAmount = numberF;
+        }
+        Invoke("BeginAnim", delay);
+    }
+
+    void BeginAnim()
+    {
+        this.anim = true;
+    }
+
+
+
+    private void Update()
+    {
+        if (anim)
+        {
+            if (img_back.fillAmount >= val_destiny)
+            {
+                img_back.fillAmount = img_back.fillAmount - 1 * Time.deltaTime * speed;
+            }
+            else
+            {
+                img_back.fillAmount = val_destiny;
+                img_back.enabled = false;
+                anim = false;
+            }
+        }
     }
 
     public override void OnValueChangeWithDelay(int value, float delay, int max = 100, bool anim = false)
     {
-        StartCoroutine(DelayEffect(value, delay, max));
+        
     }
 
-    IEnumerator DelayEffect(float value, float delay, float max)
-    {
-        yield return new WaitForSeconds(delay);
-        float val = value;
-        float mx = max;
-        float numberF = (val / mx);
-        while (val > numberF)
-        {
-            val--;
-            img_back.fillAmount = val;
-            yield return new WaitForEndOfFrame();
-        }
-    }
+
+    //[SerializeField] Image img;
+    //[SerializeField] Image img_back;
+
+    //bool anim;
+    //public float speed = 0.5f;
+    //float val_destiny;
+    //public float delay = 0.5f;
+
+    //public override void OnValueChange(int value, int max = 100, bool anim = false)
+    //{
+    //    float numberF = (value / max);
+    //    val_destiny = numberF;
+    //    img.fillAmount = numberF;
+    //    //if (img_back.fillAmount < val_destiny) img_back.fillAmount = val_destiny;
+    //    //Invoke("BeginAnim", delay);
+    //}
+
+    //void BeginAnim()
+    //{
+    //    anim = true;
+    //}
+
+    //IEnumerator DelayEffect(float value, float delay, float max)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    float val = value;
+    //    float mx = max;
+    //    float numberF = (val / mx);
+    //    while (val > numberF)
+    //    {
+    //        val--;
+    //        img_back.fillAmount = val;
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
 }
