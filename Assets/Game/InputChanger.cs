317775b8c0c07f0658ce3167fe7c623b594ee7e0
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-06-26 21:47:09 -0300

    cartelitos dinamico con database

--- Assets/Game/InputChanger.cs
+++ Assets/Game/InputChanger.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 
 public class InputChanger : MonoBehaviour
 {
-    public Sprite sprt;
+    public SpriteRenderer sprt;
     public Image img;
     public InputImageDatabase.InputImageType type;
 
     public void Start()
     {
         Main.instance.eventManager.SubscribeToEvent(GameEvents.CHANGE_INPUT, OnChange);
+
+        if (img) img.sprite = InputImageDatabase.instance.GetSprite(type);
+        if (sprt) sprt.sprite = InputImageDatabase.instance.GetSprite(type);
     }
 
     public void OnChange(params object[] p)
     {
         string val = (string)p[0];
-        if(img) img.sprite = InputImageDatabase.instance.GetSprite(type);
+        if (img) img.sprite = InputImageDatabase.instance.GetSprite(type);
+        if (sprt) sprt.sprite = InputImageDatabase.instance.GetSprite(type);
     }
 }
