c597839f1c831a29cf4c955d1e4c55e1940c5320
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-06-27 02:47:23 -0300

    Teleport variante con particula y cambio de escena

---
+++ Assets/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public void Start()
+    {
+        var aux = FindObjectsOfType<DontDestroy>();
+        for (int i = 0; i < aux.Length; i++)
+        {
+            Destroy(aux[i].gameObject);
+        }
+    }
+
+    public void BTN_Replay()
+    {
+        Scenes.Load_Load();
+    }
+    public void BTN_EndGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Game/Area/Scripts/UIs/3DUI/3DLife/Stats2D_Bar.cs
+++ Assets/Game/Area/Scripts/UIs/3DUI/3DLife/Stats2D_Bar.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 
 public class Stats2D_Bar : FrontendStatBase
 {
-    private Image img;
+    [SerializeField] Image img;
+    [SerializeField] Image img_back;
     private void Start()
     {
-        img = GetComponent<Image>();
+       // img = GetComponent<Image>();
     }
+
+
+
 
     public override void OnValueChange(int value, int max = 100, bool anim = false)
     {
@@ -24,6 +28,20 @@ public class Stats2D_Bar : FrontendStatBase
 
     public override void OnValueChangeWithDelay(int value, float delay, int max = 100, bool anim = false)
     {
-        
+        StartCoroutine(DelayEffect(value, delay, max));
+    }
+
+    IEnumerator DelayEffect(float value, float delay, float max)
+    {
+        yield return new WaitForSeconds(delay);
+        float val = value;
+        float mx = max;
+        float numberF = (val / mx);
+        while (val > numberF)
+        {
+            val--;
+            img_back.fillAmount = val;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
