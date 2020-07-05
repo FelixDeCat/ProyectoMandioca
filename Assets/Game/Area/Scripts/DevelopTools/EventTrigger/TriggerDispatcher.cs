0e748da6a4e0ace87670d11f09d81c84d6df8b22
Agezma <Agezma@live.com>
2020-07-05 19:48:59 -0300

    Puerta - InstaDash (test)

--- Assets/Game/Area/Scripts/DevelopTools/EventTrigger/TriggerDispatcher.cs
+++ Assets/Game/Area/Scripts/DevelopTools/EventTrigger/TriggerDispatcher.cs
@@ -6,18 +6,47 @@ using UnityEngine.Events;
 public class TriggerDispatcher : MonoBehaviour
 {
     [SerializeField] UnityEvent OnTriggerEnterEvent;
+    [SerializeField] UnityEvent OnTriggerStayEvent;
+    [SerializeField] UnityEvent OnTriggerExitEvent;
+    [SerializeField] UnityEvent OnTriggerLateEnterEvent;
+
     [SerializeField] TriggerReceiver[] receivers;
+
+    [SerializeField] Entities[] entitiesCanTrigger;
 
     private void OnTriggerEnter(Collider other)
     {
+        OnExecute( OnTriggerEnterEvent,other);
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        OnExecute(OnTriggerStayEvent, other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnExecute(OnTriggerExitEvent, other);
+    }
+
+    void OnExecute(UnityEvent eventToInvoke, Collider other)
+    {
         if (other.gameObject.GetComponent<CharacterHead>() != null)
         {
-            OnTriggerEnterEvent.Invoke();
+            eventToInvoke.Invoke();
 
             foreach (var r in receivers)
             {
                 r.Execute();
             }
-        }
+        }               
     }
+
+}
+
+public enum Entities //In progress
+{
+    all,
+    player,
+    enemy
 }
