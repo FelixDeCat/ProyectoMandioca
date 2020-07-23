6ada352fccac1f3a04ae27c335b19899338ac579
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-07-23 19:53:16 -0300

    Talkable script + prefab

---
+++ Assets/Game/Area/Scripts/Dialogue/TalkableModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkableModule : Interactable
+{
+    public override void OnExecute(WalkingEntity collector)
+    {
+        //aca ejecutas el dialogo
+    }
+
+    #region Mensaje
+    public override void OnEnter(WalkingEntity entity)
+    {
+        WorldItemInfo.instance.Show(pointToMessage.position, "tittle","desc","Hablar", false, false);
+    }
+    public override void OnExit()
+    {
+        WorldItemInfo.instance.Hide();
+    }
+    #endregion
+}
