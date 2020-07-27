ca9a5de2aff508df6b1e5f27d74f8ac9fd6e3812
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2020-07-27 19:15:31 -0300

    pilar que cae

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/FallenPillar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenPillar : MonoBehaviour
+{
+    DamageReceiver dmg;
+    [SerializeField] GameObject pillarFallen;
+    [SerializeField] GameObject originPillar;
+    _Base_Life_System _BLS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dmg = GetComponent<DamageReceiver>();
+        _BLS = GetComponent<_Base_Life_System>();
+        originPillar.SetActive(true);
+        pillarFallen.SetActive(false);
+        _BLS.Initialize();
+        _BLS.CreateADummyLifeSystem();
+        dmg.Initialize(
+            transform,
+            () => { return false; },
+            (x) => { },
+            (x) => { GetHit(); },
+            GetComponent<Rigidbody>(),
+            _BLS);
+    }
+
+    
+
+    void GetHit()
+    {
+        originPillar.SetActive(false);
+        pillarFallen.SetActive(true);
+    }
+}
