a26dfea47884721a86b4158b1ae0419bd605cb4d
Agezma <Agezma@live.com>
2020-08-03 18:55:19 -0300

    Enemy que stunea :bomb:

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/CombatArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CombatArea : MonoBehaviour
+{
+    public bool isCircle;
+    public Vector2 cubeArea;
+    public float circleRadius;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (isCircle)
+            Gizmos.DrawWireSphere(transform.position, circleRadius);
+        else
+            Gizmos.DrawWireCube(transform.position, new Vector3(cubeArea.x, 1, cubeArea.y));
+    }
+}
