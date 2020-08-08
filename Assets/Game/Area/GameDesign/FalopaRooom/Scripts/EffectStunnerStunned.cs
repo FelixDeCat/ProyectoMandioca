30170a7682d5d4d1780fb5bc26594c458ec95a52
Agezma <Agezma@live.com>
2020-08-07 21:10:47 -0300

    El enemy stuner se fue stuneado :hammer:

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/EffectStunnerStunned.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EffectStunnerStunned : EffectBase
+{
+    public UnityEvent startStun;
+    public UnityEvent endStun;
+
+    protected override void OffEffect()
+    {
+        endStun.Invoke();
+    }
+
+    protected override void OnEffect()
+    {
+        startStun.Invoke();
+    }
+
+    protected override void OnTickEffect(float cdPercent)
+    {
+
+    }
+}
--- Assets/Game/Area/GameDesign/FalopaRooom/Scripts/EnemyStunner.cs
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/EnemyStunner.cs
@@ -25,15 +25,21 @@ public class EnemyStunner : WalkingEntity
     CharacterHead myChar;
     EffectReceiver charEffect;
 
+    EffectReceiver myEffects;
+
+    EnemyStunnerStates currentState = EnemyStunnerStates.Idle;
+
     void Start()
     {
         OnInitialize();
     }
     private void Update()
     {
-        if (isCasting && Vector3.Distance(myChar.transform.position, transform.position) > range)
+        if (currentState == EnemyStunnerStates.Casting && Vector3.Distance(myChar.transform.position, transform.position) > range)
+        {
             castingBar.InterruptCasting();
-
+            currentState = EnemyStunnerStates.Idle;
+        }
     }
     protected override void OnInitialize()
     {
@@ -44,6 +50,7 @@ public class EnemyStunner : WalkingEntity
         life = GetComponent<EnemyLifeSystem>();
         castingBar = GetComponent<CastingBar>();
         damageReceiver = GetComponent<DamageReceiver>();
+        myEffects = GetComponent<EffectReceiver>();
         castTimer = TimeBetweenCasts;
 
         life.Initialize(life.life, () => { }, () => { }, () => { });
@@ -56,34 +63,33 @@ public class EnemyStunner : WalkingEntity
             rb,
             life
             );
-        StartCoroutine(Stun());
+        StartCoroutine(StartCastStun());
 
-        castingBar.AddEventListener_OnStartCasting( ()=> IsCasting(true) );
+        castingBar.AddEventListener_OnStartCasting(() => ChangeState(EnemyStunnerStates.Casting));
 
-        castingBar.AddEventListener_OnInterruptCasting(StartStun);
-        castingBar.AddEventListener_OnInterruptCasting(() => IsCasting(false));
+        castingBar.AddEventListener_OnInterruptCasting(StartStun);        
 
         castingBar.AddEventListener_OnFinishCasting(StunChar);
-        castingBar.AddEventListener_OnFinishCasting(() => IsCasting(false));
+        castingBar.AddEventListener_OnFinishCasting(() => ChangeState(EnemyStunnerStates.Idle));
     }
-    
+
+
+    void ChangeState(EnemyStunnerStates state)
+    {
+        currentState = state;
+    }
+
     void StartStun()
     {
-        StartCoroutine(Stun());
+        StartCoroutine(StartCastStun());
     }
 
-    IEnumerator Stun()
+    IEnumerator StartCastStun()
     {
         yield return new WaitForSeconds(TimeBetweenCasts);
-        while (Vector3.Distance(transform.position, myChar.transform.position) > range)
+        while (Vector3.Distance(transform.position, myChar.transform.position) > range || currentState != EnemyStunnerStates.Idle)
             yield return new WaitForSeconds(0.1f);
-        castingBar.StartCasting();
-    }
-
-    bool isCasting = false;
-    void IsCasting(bool casting)
-    {
-        isCasting = casting;
+        castingBar.StartCasting();  
     }
 
     void StunChar()
@@ -92,20 +98,36 @@ public class EnemyStunner : WalkingEntity
         StartStun();
     }
 
-    void TakeDamage()
+    public void GetStunned()
     {
-        castingBar.InterruptCasting();
-        Vector2 aux;
-        if(spawneablePosition.isCircle)
+        if(currentState == EnemyStunnerStates.Casting)
         {
-            float ang = Random.Range(0,360);
+            castingBar.InterruptCasting();
+            ChangeState(EnemyStunnerStates.Stunned);
+        }
+    }
+    public void EndStun()
+    {
+        if (currentState == EnemyStunnerStates.Stunned)
+        {
+            ChangeState(EnemyStunnerStates.Idle);
+            Teleport();
+        }
+    }
+
+    void Teleport()
+    {
+        Vector2 aux;
+        if (spawneablePosition.isCircle)
+        {
+            float ang = Random.Range(0, 360);
 
             Vector3 pos;
             pos.x = spawneablePosition.transform.position.x + spawneablePosition.circleRadius * Random.value * Mathf.Sin(ang * Mathf.Deg2Rad);
             pos.y = spawneablePosition.transform.position.y;
             pos.z = spawneablePosition.transform.position.z + spawneablePosition.circleRadius * Random.value * Mathf.Cos(ang * Mathf.Deg2Rad);
 
-            transform.position = pos;            
+            transform.position = pos;
         }
         else
         {
@@ -113,6 +135,15 @@ public class EnemyStunner : WalkingEntity
             aux.y = -(spawneablePosition.cubeArea.y / 2) + spawneablePosition.cubeArea.y * Random.value;
             transform.position = spawneablePosition.transform.position + new Vector3(aux.x, 0, aux.y);
         }
+    }
+
+    void TakeDamage()
+    {
+        if (currentState == EnemyStunnerStates.Stunned) return;
+
+        ChangeState(EnemyStunnerStates.Idle);
+        castingBar.InterruptCasting();
+        Teleport();          
     }
 
     void Die()
@@ -125,8 +156,8 @@ public class EnemyStunner : WalkingEntity
     {
         Gizmos.DrawWireSphere(transform.position, range);
     }
-       
-    protected override void OnUpdateEntity() {}
+
+    protected override void OnUpdateEntity() { }
     protected override void OnTurnOn()
     {
     }
@@ -142,4 +173,9 @@ public class EnemyStunner : WalkingEntity
     protected override void OnResume()
     {
     }
+}
+
+public enum EnemyStunnerStates
+{
+    Idle, Casting, Stunned
 }
