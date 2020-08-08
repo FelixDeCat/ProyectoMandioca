3589338a4f53ecbe98e3d174c7f73606c78c364d
Frano <frano.criscuolo@gmail.com>
2020-08-07 21:39:16 -0300

    Cambio incio isla y mega animacion escudo

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/ShieldFalling_AnimHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShieldFalling_AnimHelper : MonoBehaviour
+{
+    public UnityEvent e;
+
+    public void StartFallingShield()
+    {
+        GetComponent<Animator>().SetTrigger("FallingShield");
+
+        
+    }
+
+    public void FallingFloor()
+    {
+        e?.Invoke();
+    }
+
+
+}
