167d7291725a9d5edca60465085298071db2d33f
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2020-08-13 21:22:28 -0300

    camara contra pared

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/DetectorOfCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorOfCamera : MonoBehaviour
+{
+    [SerializeField]RotateTheCamera _myCamera;
+    bool colliding;
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 21)
+        {
+            _myCamera.ReturnToPos(true);
+
+
+        }
+            
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 21)
+            _myCamera.ReturnToPos(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 21)
+            _myCamera.ReturnToPos(false);
+
+    }
+}
--- Assets/Game/Area/GameDesign/FalopaRooom/Scripts/RotateTheCamera.cs
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/RotateTheCamera.cs
@@ -6,24 +6,27 @@ public class RotateTheCamera : MonoBehaviour
 {
 
     public GameObject target;
+    bool _returnToPos;
     [SerializeField] float _speed;
+    [SerializeField] float _speedToReturn;
+    private float PosY;
 
+    private void Start()
+    {
+        PosY = transform.position.y;
+    }
 
     private void Update()
     {
-        //if (Input.GetKey(KeyCode.Z))
-        //{
-        //    RotationOfCamera(1f);
-        //}
-        //if (Input.GetKey(KeyCode.X))
-        //{
-        //    RotationOfCamera(-1f);
-        //}
-        //else
-        //    RotationOfCamera(0);
+        if (_returnToPos)
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, PosY, transform.position.z), _speedToReturn * Time.deltaTime);
     }
     public void RotationOfCamera(float axis)
     {
         transform.RotateAround(target.transform.position, Vector3.up, axis *_speed* Time.deltaTime);
+    }
+    public void ReturnToPos(bool result)
+    {
+        _returnToPos = result;
     }
 }
