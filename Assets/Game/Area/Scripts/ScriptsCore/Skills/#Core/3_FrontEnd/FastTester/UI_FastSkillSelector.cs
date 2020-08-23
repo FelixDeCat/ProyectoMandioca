4a296170115140049186c4122d9d2d844d1d1318
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-08-22 22:30:08 -0300

    algunos arreglos

--- Assets/Game/Area/Scripts/ScriptsCore/Skills/#Core/3_FrontEnd/FastTester/UI_FastSkillSelector.cs
+++ Assets/Game/Area/Scripts/ScriptsCore/Skills/#Core/3_FrontEnd/FastTester/UI_FastSkillSelector.cs
@@ -13,23 +13,23 @@ public class UI_FastSkillSelector : UI_Base
 
     public void Build(List<SkillBase> collection, Action<int, bool> _callbackSelection)
     {
-        parent.SetActive(true);
-        for (int i = 0; i < collection.Count; i++)
-        {
-            GameObject go = Instantiate(model, parent_rect);
-            var elem = go.GetComponent<UI_FastSkillSelectorElement>();
-            elem.SetInfo(
-                collection[i].skillinfo.skill_name,
-                collection[i].skillinfo.description_technical,
-                collection[i].skillinfo.img_actived,
-                i,
-                _callbackSelection,
-                GetOnImage,
-                GetOffImage);
+    //    parent.SetActive(true);
+    //    for (int i = 0; i < collection.Count; i++)
+    //    {
+    //        GameObject go = Instantiate(model, parent_rect);
+    //        var elem = go.GetComponent<UI_FastSkillSelectorElement>();
+    //        elem.SetInfo(
+    //            collection[i].skillinfo.skill_name,
+    //            collection[i].skillinfo.description_technical,
+    //            collection[i].skillinfo.img_actived,
+    //            i,
+    //            _callbackSelection,
+    //            GetOnImage,
+    //            GetOffImage);
 
-            elements.Add(elem);
-        }
-        parent.SetActive(false);
+    //        elements.Add(elem);
+    //    }
+    //    parent.SetActive(false);
     }
 
     protected override void OnAwake() { }
