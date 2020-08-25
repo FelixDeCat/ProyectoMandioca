e341422dda2eddf97abda74c9b606ddbdb3dbc3a
Frano <frano.criscuolo@gmail.com>
2020-08-25 10:39:16 -0300

    Ramitas pinchan

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/SpikeCorruptedVine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCorruptedVine : MonoBehaviour
+{
+    DamageData damageData;
+
+    private void Start()
+    {
+        damageData = GetComponent<DamageData>();
+
+        damageData.SetDamage(2).SetDamageType(Damagetype.NonBlockAndParry);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterHead>())
+        {
+            DamageReceiver character = other.gameObject.GetComponent<DamageReceiver>();
+
+            character.TakeDamage(damageData);
+        }
+    }
+}
