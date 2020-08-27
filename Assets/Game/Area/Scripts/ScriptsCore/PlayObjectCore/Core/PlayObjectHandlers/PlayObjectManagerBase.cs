7b11e4896a5d150cb9083b9e8c9917cf6ec6d35a
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-08-27 20:42:43 -0300

    escena "PARAMOSTRAR" con PlayObjectManager

--- Assets/Game/Area/Scripts/ScriptsCore/PlayObjectCore/Core/PlayObjectHandlers/PlayObjectManagerBase.cs
+++ Assets/Game/Area/Scripts/ScriptsCore/PlayObjectCore/Core/PlayObjectHandlers/PlayObjectManagerBase.cs
@@ -1,11 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public abstract class PlayObjectManagerBase
+/////////////////////////////////////////////////////////////
+/// LOAD COMPONENT, NO OLVIDAR
+/////////////////////////////////////////////////////////////
+
+
+public class PlayObjectManagerBase : LoadComponent
 {
-    //esta clase esta pensada a largo plazo
-    //solo fue creada con la intencion de 
-    //maquetar la estructura y reservar los nombres
-    //No Borrar
+    [SerializeField] Transform parent;
+    [SerializeField] List<PlayObject> playobjects = new List<PlayObject>();
+    Action callbackSubscribeNewChanges;
+
+    protected override IEnumerator LoadMe()
+    {
+        yield return Check(parent);
+    }
+
+    public void SubscribeMeToNewChanges(Action callback_new_changes) => callbackSubscribeNewChanges = callback_new_changes;
+
+    IEnumerator Check(Transform t)
+    {
+        var count = t.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            var child = t.GetChild(i);
+            var obj = child.gameObject.GetComponent<PlayObject>();
+            if (obj != null)
+                if (!playobjects.Contains(obj))
+                    playobjects.Add(obj);
+            StartCoroutine(Check(child));
+            yield return null;
+        }
+    }
+    public void AddPlayObject(PlayObject _obj)
+    {
+        if (!playobjects.Contains(_obj))
+        {
+            playobjects.Add(_obj);
+            callbackSubscribeNewChanges.Invoke();
+        }
+    }
+    public void RemovePlayObject(PlayObject _obj)
+    {
+        if (!playobjects.Contains(_obj))
+        {
+            playobjects.Remove(_obj);
+            callbackSubscribeNewChanges.Invoke();
+        }
+    }
 }
