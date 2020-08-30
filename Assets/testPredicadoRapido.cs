7f6f35eb5f4127e5fb865f81cfa45654ea1533dd
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-08-30 17:52:36 -0300

    test predicado rapido, borrar cuando se termine de usar

---
+++ Assets/testPredicadoRapido.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools.EventClasses;
+
+public class testPredicadoRapido : MonoBehaviour
+{
+    public EventCounterPredicate pred;
+
+    private void Start()
+    {
+        pred.Invoke(MyPred);
+    }
+
+    bool MyPred()
+    {
+        return true;
+    }
+}
