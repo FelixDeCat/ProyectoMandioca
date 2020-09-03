e517f6a976815076e3b838bec98a131569213998
Gonzalou <56851977+Gonzalou@users.noreply.github.com>
2020-09-03 00:22:13 -0300

    Hice sarpado script gente #CodeMaster :sunglasses:

---
+++ Assets/Game/Area/Art/Characters/Ent/SeguimeloAEste.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimeloAEste : MonoBehaviour
+{
+    public GameObject ObjASeguir;    
+   
+    // Para cuando no queres poner de hijo algun gameObject
+    void Update()
+    {
+        transform.position = ObjASeguir.transform.position;
+        transform.rotation = ObjASeguir.transform.rotation;
+    }
+}
