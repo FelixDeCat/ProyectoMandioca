e771b4eea3b3d664211669f23c82ace67c20554f
Frano <frano.criscuolo@gmail.com>
2020-09-07 11:43:47 -0300

    HealthPickUp Berries (a comer bayas!)

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/HealthBerries.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBerries : MonoBehaviour
+{
+    [SerializeField] int healthAmount;
+
+    [SerializeField] ParticleSystem feedback;
+
+   public void ConsumeBerries()
+    {
+        if (feedback != null) feedback.Play();
+
+        Main.instance.GetChar().Life.Heal(healthAmount);
+    }
+}
