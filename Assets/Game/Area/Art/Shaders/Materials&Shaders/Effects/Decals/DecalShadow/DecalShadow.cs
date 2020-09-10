4f35b7a223fa6a54d80732843e1771c31a3453d1
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-09-10 19:07:24 -0300

    Ahhhhhhhhh maldito bakee

---
+++ Assets/Game/Area/Art/Shaders/Materials&Shaders/Effects/Decals/DecalShadow/DecalShadow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class DecalShadow : MonoBehaviour
+{
+    public Material mat;
+
+
+    private void Update()
+    {
+        mat.SetVector("_Pos", transform.position);
+    }
+}
