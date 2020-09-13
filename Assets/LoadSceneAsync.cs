5f8a2c86eabfb8de969793d8a3ab49f0521b9269
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-09-13 02:14:58 -0300

    load async scene un poco mas pulida

--- Assets/LoadSceneAsync.cs
+++ Assets/LoadSceneAsync.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSceneAsync : MonoBehaviour
 {
+    string SceneToLoad;
     public void BeginLoad(string scene)
     {
-        LoadSceneHandler.instance.LoadAScene(scene, false);
+        SceneToLoad = scene;
+        StartCoroutine(LoadAsyncScene());
     }
 
-    public void ClearLoad()
+    AsyncOperation asyncLoad;
+    IEnumerator LoadAsyncScene()
     {
+        bool canLoad = true;
 
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneToLoad == SceneManager.GetSceneAt(i).name)
+            {
+                canLoad = false;
+            }
+        }
+
+        if (!canLoad) yield break;
+
+        if (SceneManager.GetActiveScene().name != SceneToLoad)
+        {
+            
+            asyncLoad = SceneManager.LoadSceneAsync(SceneToLoad, LoadSceneMode.Additive);
+
+            asyncLoad.allowSceneActivation = true;
+
+            yield return null;
+        }
     }
 }
