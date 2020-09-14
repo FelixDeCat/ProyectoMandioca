638934d3584d672cb38570c2cc895f6b1679fc80
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-09-14 19:00:16 -0300

    PP, shaders y decepcion :|

---
+++ Assets/Game/Area/Art/Shaders/Materials&Shaders/Effects/Infinite/RTSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class RTSettings : MonoBehaviour
+{
+
+    public CustomRenderTexture tempRender;
+
+    public Camera secondaryCamera;
+    public Camera mainCamera;
+
+    public Material mat;
+
+    private void Awake()
+    {
+        tempRender = new CustomRenderTexture(mainCamera.pixelWidth, mainCamera.pixelHeight, RenderTextureFormat.ARGB32);
+
+        tempRender.Create();
+
+     
+
+        secondaryCamera.targetTexture = tempRender;
+
+        mat.SetTexture("_RT", tempRender);
+    }
+}
