ab79cf0eebf3f7b530f04fc81b8bcc1d262bd2de
Gonzalou <56851977+Gonzalou@users.noreply.github.com>
2020-09-27 01:29:06 -0300

    Agregue un chorizo gigante en la escena de caronte

---
+++ Assets/CintaTransportadora.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CintaTransportadora : MonoBehaviour
+{
+    public Transform endpos;
+    public Transform inipos;
+    public Vector3 dirTogo;
+    public float speed;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(endpos.position,transform.position) <= 10)
+        {
+            transform.position = inipos.position;
+        }
+        transform.position += dirTogo * speed * Time.deltaTime;
+    }
+}
