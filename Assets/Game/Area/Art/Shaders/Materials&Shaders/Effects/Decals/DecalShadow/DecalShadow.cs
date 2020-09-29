574db226d17989b4a6a2d4636ff715d91d560b25
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-09-28 22:12:25 -0300

    Agua y sombra

--- Assets/Game/Area/Art/Shaders/Materials&Shaders/Effects/Decals/DecalShadow/DecalShadow.cs
+++ Assets/Game/Area/Art/Shaders/Materials&Shaders/Effects/Decals/DecalShadow/DecalShadow.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
+
 public class DecalShadow : MonoBehaviour
 {
-    public Material mat;
+    private Material mat;
+    public GameObject floor;
+
+
+    private void Awake()
+    {
+
+        mat = floor.GetComponent<MeshRenderer>().material;
+       
+
+    }
 
 
     private void Update()
     {
+        if (mat == null) return;
         mat.SetVector("_Pos", transform.position);
+
     }
+
+   
+
 }
