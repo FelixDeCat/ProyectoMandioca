1f6d5f182824a53e2c825e06f5648f6118cf5f52
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-09-29 20:42:30 -0300

    z2 prefabs + refes de otras escenas

--- Assets/Game/#Scenes/LoadByScene/LocalSceneHandler.cs
+++ Assets/Game/#Scenes/LoadByScene/LocalSceneHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngineInternal;
 
@@ -52,13 +53,10 @@ public class LocalSceneHandler : LoadComponent
         {
             case SceneData.Detail_Parameter.none: break;
             case SceneData.Detail_Parameter.full_load:
-                yield return new WaitForSecondsRealtime(0.3f);
-                if ( SceneData.low_detail) this.low_detail = Instantiate(SceneData.low_detail);
-                yield return new WaitForSecondsRealtime(0.3f);
-                if (SceneData.medium_detail) this.medium_detail = Instantiate(SceneData.medium_detail);
-                yield return new WaitForSecondsRealtime(0.3f);
-                if (SceneData.hight_detail) this.hight_detail = Instantiate(SceneData.hight_detail);
-                yield return null;
+                yield return Inst(gameplay, PrefabType.gameplay);
+                yield return ExecuteSwitching(low_detail, ExeParam.show, PrefabType.low);
+                yield return ExecuteSwitching(medium_detail, ExeParam.show, PrefabType.med);
+                yield return ExecuteSwitching(hight_detail, ExeParam.show, PrefabType.high);
                 break;
             case SceneData.Detail_Parameter.top_to_landmark: break;
             case SceneData.Detail_Parameter.top_to_low: break;
@@ -66,6 +64,46 @@ public class LocalSceneHandler : LoadComponent
             case SceneData.Detail_Parameter.destroy_and_go_landmark: break;
             case SceneData.Detail_Parameter.destroy_and_go_low: break;
             case SceneData.Detail_Parameter.destroy_and_go_medium: break;
+        }
+        yield return null;
+    }
+    enum ExeParam { show, shutdown, destroy }
+    enum PrefabType { landmark, low, med, high, gameplay }
+
+    IEnumerator ExecuteSwitching(GameObject go, ExeParam exe, PrefabType preftype)
+    {
+        if (exe == ExeParam.show)
+        {
+            if (go != null)
+            {
+                go.SetActive(true);
+            }
+            else
+            {
+                yield return new WaitForSecondsRealtime(0.3f);
+                yield return Inst(go, preftype);
+                yield return null;
+            }
+        }
+        else if (exe == ExeParam.shutdown)
+        {
+            if (go != null) go.SetActive(false);
+        }
+        else
+        {
+            if (go != null) Destroy(go);
+        }
+    }
+
+    IEnumerator Inst(GameObject go, PrefabType prefType)
+    {
+        switch (prefType)
+        {
+            case PrefabType.gameplay: if (SceneData.gameplay) go = Instantiate(SceneData.gameplay); yield return null; break;
+            case PrefabType.landmark: if (SceneData.landmark) go = Instantiate(SceneData.landmark); yield return null; break;
+            case PrefabType.low: if (SceneData.low_detail) go = Instantiate(SceneData.low_detail); yield return null; break;
+            case PrefabType.med: if (SceneData.medium_detail) go = Instantiate(SceneData.medium_detail); yield return null; break;
+            case PrefabType.high: if (SceneData.hight_detail) go = Instantiate(SceneData.hight_detail); yield return null; break;
         }
     }
 
