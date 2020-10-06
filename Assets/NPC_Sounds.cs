1f78c17c6f37d743c086990d83d4f68c329bf9fd
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-10-06 20:18:57 -0300

    jacinta preparada para sonidos

---
+++ Assets/NPC_Sounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Sounds : MonoBehaviour
+{
+    public string npc_Name = "Jacinta";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Command
+               .AddBranch(new CommandBranch("NPC")
+                   .AddBranch(new CommandBranch("Sound")
+                       .AddBranch(new CommandBranch("Play")
+                           .AddBranch(new CommandBranch(npc_Name)
+                           .AddLeaf(Play_Cry,"Cry")
+                           ))));
+    }
+
+    public void Play_Cry(string s)
+    {
+
+    }
+}
