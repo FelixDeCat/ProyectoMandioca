576f32dce67c05ae7d2f7aec5fdd216205a006be
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-10-07 21:00:46 -0300

    boomerang shield sound script para tobias

---
+++ Assets/BommerangShiledFeedbacks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BommerangShiledFeedbacks : MonoBehaviour
+{
+    public void Play_ThrowActive()
+    {
+    
+    }
+    public void Play_Movement()
+    {
+
+    }
+
+    public void Play_Impact()
+    {
+    
+    }
+}
