738e1a3c7bd227348237089ee9a3389099405cb3
j33zas <matias.levy@davinci.edu.ar>
2020-10-08 19:57:46 -0300

    1er pasada jaSinta

---
+++ Assets/Game/#Prefab/2_LocalPrefabs/DungeonLevy/JacintaAnimControllerCABEZA.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JacintaAnimControllerCABEZA : MonoBehaviour
+{
+    [SerializeField]
+    Animator myanim;
+    void Start()
+    {
+        myanim.SetBool("Crying", true);
+    }
+}
