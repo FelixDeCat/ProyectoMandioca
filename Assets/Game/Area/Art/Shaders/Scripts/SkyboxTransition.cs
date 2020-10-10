89b11aa35522ee4c6f6cd3f0ff158ee558c74872
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-10-09 23:07:43 -0300

    Empezando la transicion de dia a noche

---
+++ Assets/Game/Area/Art/Shaders/Scripts/SkyboxTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxTransition : MonoBehaviour
+{
+
+    public Material skybox;
+
+    [Range(0,1)]
+    public float NigthValue;
+
+    private void Update()
+    {
+        SetNigth();
+    }
+
+    void SetNigth()
+    {
+        NigthValue += Time.deltaTime*.1f;
+        skybox.SetFloat("_Night", NigthValue);
+        NigthValue = Mathf.Clamp(NigthValue, 0, 1);
+    }
+
+    void SetDay()
+    {
+
+    }
+
+    
+
+}
