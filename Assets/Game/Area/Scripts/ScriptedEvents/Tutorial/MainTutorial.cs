2b91c4dea2882d28788b66fe9828297cbc6984d8
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-10-10 13:41:16 -0300

    algunos ajustes al tutorial + preparativos para el evento de atenea

--- Assets/Game/Area/Scripts/ScriptedEvents/Tutorial/MainTutorial.cs
+++ Assets/Game/Area/Scripts/ScriptedEvents/Tutorial/MainTutorial.cs
@@ -6,54 +6,41 @@ public class MainTutorial : MonoBehaviour
 {
     public const int ID_TUTORIAL = 4;
     public const int FASE_LIBERA_CAMINO_JACINTA = 0;
-    public const int FASE_ACTIVO_DOCTOR = 1;
-    public const int FASE_PLANTAS_ENTREGADAS = 2;
-    public const int FASE_ATENEA_DEJA_PASAR = 3;
+    public const int FASE_CAMINO_A_DOCTOR_DESPEJADO = 1;
+    public const int FASE_ACTIVO_DOCTOR = 2;
+    public const int FASE_PLANTAS_ENTREGADAS = 3;
+    public const int FASE_ATENEA_DEJA_PASAR = 4;
 
     public ObjetiveSubscriber jacinta_ents_Objetive;
     public ObjetiveSubscriber farm_armored_ent_Objetive;
+    public ObjetiveSubscriber doctorway_ents_Objetive;
     public int Plants_Recollection_ID;
-    bool doc_req_armored_ent_killed = false;
-    bool doc_req_plants_mision_ended = false;
+    bool objetive_armored_ent = false;
+    bool objetive_plants = false;
 
     public void TRIGGER_StartTutorial()
     {
         ChangeFase(FASE_LIBERA_CAMINO_JACINTA);
-        jacinta_ents_Objetive.BeginObjetive(All_Jacinta_Ents_Killed);
+
+        jacinta_ents_Objetive.BeginObjetive(FinishObjetive_All_Jacinta_Ents_Killed);
+        farm_armored_ent_Objetive.BeginObjetive(FinishObjetive_Armored_Ent_Killed);
+        doctorway_ents_Objetive.BeginObjetive(FinishObjetive_Way_To_Doctor_Cleared);
     }
 
-    void All_Jacinta_Ents_Killed()
-    {
-        ChangeFase(FASE_ACTIVO_DOCTOR);
-        farm_armored_ent_Objetive.BeginObjetive(Armored_Ent_Killed);
-    }
+    //externo por UnityEvent
+    public void BeginPlantsMision() => MisionManager.instancia.AddMision(MisionsDataBase.instance.GetMision(Plants_Recollection_ID), FinishObjetive_Curative_Plants_Recollected);
 
-    public void BeginPlantsMision()
-    {
-        MisionManager.instancia.AddMision(MisionsDataBase.instance.GetMision(Plants_Recollection_ID), All_Curative_Plants_Recollected);
-    }
-
-    void All_Curative_Plants_Recollected(int ID)
-    {
-        Debug.Log("Plantas recolectadas");
-        doc_req_plants_mision_ended = true;
-        DoctorRequirements();
-    }
-    void Armored_Ent_Killed()
-    {
-        Debug.Log("Ent con armadura matado");
-        doc_req_armored_ent_killed = true;
-        DoctorRequirements();
-    }
-
+    void FinishObjetive_All_Jacinta_Ents_Killed() => ChangeFase(FASE_ACTIVO_DOCTOR);
+    void FinishObjetive_Way_To_Doctor_Cleared() => ChangeFase(FASE_CAMINO_A_DOCTOR_DESPEJADO);
+    void FinishObjetive_Curative_Plants_Recollected(int ID) { objetive_plants = true; DoctorRequirements(); }
+    void FinishObjetive_Armored_Ent_Killed() { objetive_armored_ent = true; DoctorRequirements(); }
     void DoctorRequirements()
     {
-        if (doc_req_armored_ent_killed && doc_req_plants_mision_ended)
+        if (objetive_armored_ent && objetive_plants)
         {
             ChangeFase(FASE_PLANTAS_ENTREGADAS);
         }
     }
-
 
     #region auxs
     void ChangeFase(int val) =>  ManagerGlobalFases.instance.ModifyFase(ID_TUTORIAL, val);
