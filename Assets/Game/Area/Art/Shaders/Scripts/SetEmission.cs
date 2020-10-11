fa21b4947eae20a8532d70ea785af6d63a307bfb
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-10-11 15:30:49 -0300

    Cosas de emission

---
+++ Assets/Game/Area/Art/Shaders/Scripts/SetEmission.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetEmission : MonoBehaviour
+{
+    public Material material;
+
+    [Range(0,1)]
+    public float EmissionIntensity;
+
+    private void Awake()
+    {
+        var sceneName = SceneManager.GetActiveScene();
+
+
+        if (sceneName.name == "LevyDungeon")
+        {
+            material.SetFloat("_EmissionIntensity", EmissionIntensity);
+        }
+        else
+            material.SetFloat("_EmissionIntensity", 0);
+
+
+    }
+
+
+}
