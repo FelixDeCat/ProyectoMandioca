803b1a55c0399b0f0cdc70e7ffa7ae9a3698f163
Frano <frano.criscuolo@gmail.com>
2020-10-12 16:54:23 -0300

    El entusiasta ahora te abre paso. Faltan cosas de la primer parte, pero lo mas pesado ya esta

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/EntusiastaRompiendoRamas.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools.Extensions;
+
+public class EntusiastaRompiendoRamas : PistonWithSteps
+{
+    [SerializeField] Transform _rootRot;
+    [SerializeField] Animator _anim;
+    [SerializeField] AnimEvent _animEvent;
+
+    private void Start()
+    {
+        OnReachDestination += () => StopRunningAndAttack();
+        _animEvent.Add_Callback("RomperRamita", RomperRamas);
+    }
+
+    public void StartWalkingAnim()
+    {
+        _anim.SetTrigger("run");
+    }
+
+    public void StopRunningAndAttack()
+    {
+        _anim.SetTrigger("stopRunning");
+        _anim.SetTrigger("Attack");
+    }
+
+    public override void Move()
+    {
+        _root.transform.position += _root.forward * speed * Time.fixedDeltaTime;
+        _rootRot.LookAt(nodes[currentNode]);
+    }
+
+    public void RomperRamas()
+    {
+       var ramitas = Extensions.FindInRadius<PropDestructible>(_rootRot.position, 5);
+
+       var data = new DamageData().SetDamage(5).SetDamageType(Damagetype.Normal);
+
+        foreach (PropDestructible item in ramitas)
+        {
+            item.GetComponent<DamageReceiver>().TakeDamage(data);
+        }
+    }
+}
