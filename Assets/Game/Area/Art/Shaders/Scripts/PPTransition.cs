52c7bc826bfc1138ef80b176799564a51cac8068
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-10-14 22:29:04 -0300

    Una cosa para que transicione cosas que sigo mañana

---
+++ Assets/Game/Area/Art/Shaders/Scripts/PPTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPTransition : MonoBehaviour
+{
+    public PostProcessVolume volumeEntry;
+    public PostProcessVolume volumeExit;
+
+    public float transitionSpeed;
+
+    public void Transition()
+    {
+       // while (volumeEntry.weight<1 && volumeExit.weight>0)
+      //  {
+            volumeEntry.weight = Time.deltaTime + transitionSpeed;
+            volumeExit.weight = Time.deltaTime - transitionSpeed;
+     //   }
+       
+
+    }
+
+}
