429c9757275355d9b6dd2848f2f6fcd22c421682
Frano <frano.criscuolo@gmail.com>
2020-10-16 20:32:00 -0300

    El doctor la pasa mal

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/EVENT_Object_Initializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVENT_Object_Initializer : MonoBehaviour
+{
+    [SerializeField] PlayObject[] objetsToInit = new PlayObject[0];
+
+    private void Start()
+    {
+        InitializeObjects();
+    }
+
+    public void InitializeObjects()
+    {
+        for (int i = 0; i < objetsToInit.Length; i++)
+        {
+            objetsToInit[i].Initialize();
+            objetsToInit[i].On();
+        }
+    }
+
+    public void EntsGoToKillPlayer()
+    {
+        for (int i = 0; i < objetsToInit.Length; i++)
+        {
+            objetsToInit[i].GetComponentInChildren<Animator>().Play("idle");
+        }
+    }
+
+}
