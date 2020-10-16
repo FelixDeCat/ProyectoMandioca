2194706f74859392d81a2f80a8fef5d30588b9ab
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2020-10-15 21:55:53 -0300

    shakironil patea

--- Assets/Game/Area/Scripts/World/Entities/Enemys/Wendigo/WendigoMelee.cs
+++ Assets/Game/Area/Scripts/World/Entities/Enemys/Wendigo/WendigoMelee.cs
@@ -7,23 +7,42 @@ namespace Tools.StateMachine
     public class WendigoMelee : WendigoStates
     {
         WendigoView view;
-        public WendigoMelee(EState<WendigoEnemy.WendigoInputs> myState, WendigoView _view, EventStateMachine<WendigoEnemy.WendigoInputs> _sm) : base(myState, _sm)
+        DamageData dmgData;
+        SphereCollider coll;
+        public WendigoMelee(EState<WendigoEnemy.WendigoInputs> myState, WendigoView _view, DamageData _dmgData, SphereCollider _coll, EventStateMachine<WendigoEnemy.WendigoInputs> _sm) : base(myState, _sm)
         {
             view = _view;
+            dmgData = _dmgData;
+            coll = _coll;
 
         }
         protected override void Enter(EState<WendigoEnemy.WendigoInputs> last)
         {
             view.Kick();
+            view.DebugText("MELEE");
         }
 
         protected override void Update()
         {
-            view.DebugText("MELEE");
-            Debug.Log("owo?");
+
+            int layerMask = LayerMask.GetMask("Player");
+            Collider[] collz = Physics.OverlapSphere(coll.transform.position, coll.radius, layerMask);
+            foreach (Collider c in collz)
+            {
+                Debug.Log(c.gameObject.name);
+                if (c.gameObject.layer == 8)
+                {
+                    dmgData.SetKnockback(1000);
+                    Debug.Log(c.gameObject.GetComponent<DamageReceiver>());
+
+                    c.gameObject.GetComponent<DamageReceiver>().TakeDamage(dmgData);
+                }
+            }
             base.Update();
             //Inserte knockback aqui
             //Daño y empujon
+            //GEtcomponentdamagereceiver 
+            //Take damage -> damage data
         }
     }
 }
