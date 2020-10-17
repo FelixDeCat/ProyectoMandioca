b2bec1fa4e1c8fc3d9c602de249eee30ebebaf5f
Frano <frano.criscuolo@gmail.com>
2020-10-16 22:07:28 -0300

    Llamen al doctor que me estoy quemando

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/Nose/PlayAnimFromString.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayAnimFromString : MonoBehaviour
+{
+    [SerializeField] string animationName;
+
+    Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+        _anim.Play(animationName);
+    }
+}
