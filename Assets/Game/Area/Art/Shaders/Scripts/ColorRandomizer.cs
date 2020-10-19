3a2e15ae2013ba1855fd01e83d1122d8fde2f216
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-10-19 19:16:45 -0300

    Arreglando y cosas del main

--- Assets/Game/Area/Art/Shaders/Scripts/ColorRandomizer.cs
+++ Assets/Game/Area/Art/Shaders/Scripts/ColorRandomizer.cs
@@ -3,49 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[ExecuteAlways]
+
 public class ColorRandomizer : MonoBehaviour
 {
-    public int randomizerWeigth;
+   
 
     public string colorProperty;
 
-    private Color _color;
-
-    public Color standardColor;
-
-    [Header("Arrays")]
-    public GameObject[] things;
+   
     public Color[] randomColors;
 
     
     private Renderer _render;
 
-   
-
-
-    public void SetArrays()
+    private void Awake()
     {
-        for (int i = 0; i < things.Length; i++)
-        {
-            _render = things[i].GetComponent<Renderer>();
-            _render.sharedMaterial.SetColor(colorProperty, Randomizer());
+        _render = GetComponent<Renderer>();
 
-        }
+
+        _render.material.SetColor(colorProperty, Randomizer());
     }
 
-    public void ClearData()
-    {
-        for (int i = 0; i < things.Length; i++)
-        {
 
-            _render = things[i].GetComponent<Renderer>();
-            _render.sharedMaterial.SetColor(colorProperty, standardColor);
-
-        }
-    }
-
-   
 
     private Color Randomizer()
     {
