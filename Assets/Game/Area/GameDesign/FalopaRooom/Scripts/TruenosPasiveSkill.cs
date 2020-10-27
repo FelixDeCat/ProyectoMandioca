1a6047d90c19e1af73468ebe09b98c9f79ad71a6
pablo Torre <pablo.torre@davinci.edu.ar>
2020-10-27 00:54:50 -0300

    version pasiva truenitos placeholder me quede sin palabras tecnicas para hacer bulto

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/TruenosPasiveSkill.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools.Extensions;
+
+public class TruenosPasiveSkill : MonoBehaviour
+{
+    [SerializeField] float range;
+    [SerializeField] float knockbackFoes;
+    [SerializeField] int damage;
+    [SerializeField] int stunTimer;
+
+    [SerializeField] Damagetype damageType = Damagetype.Normal;
+    DamageData dmgDATA;
+
+    [SerializeField] ParticleSystem particlesLindas;
+
+    CharacterHead _hero;
+
+    private void Start()
+    {
+        ParticlesManager.Instance.GetParticlePool(particlesLindas.name, particlesLindas);
+        _hero = Main.instance.GetChar();
+
+    }
+
+    private void OnEnable()
+    {
+        dmgDATA = _hero.GetComponent<DamageData>();
+        dmgDATA.Initialize(_hero);
+        dmgDATA.SetDamage(damage).SetDamageInfo(DamageInfo.NonParry).SetKnockback(knockbackFoes).SetDamageType(damageType);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button5))
+        {
+            _hero.ShieldAbilityCharge();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Joystick1Button5))
+        {
+            _hero.ShieldAbilityRelease();
+            UsePassiveSkill();
+        }
+    }
+
+    void UsePassiveSkill()
+    {
+        ParticlesManager.Instance.PlayParticle(particlesLindas.name,this.transform.position);
+
+        var enemis = Extensions.FindInRadius<DamageReceiver>(_hero.transform.position, range);
+        for (int i = 0; i < enemis.Count; i++)
+        {
+            if (enemis[i].GetComponent<EffectReceiver>() != null && enemis[i].GetComponent<EntityBase>() != _hero)
+            {
+                enemis[i].GetComponent<EffectReceiver>().TakeEffect(EffectName.OnFreeze,stunTimer);
+                enemis[i].TakeDamage(dmgDATA);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, range);
+    }
+}
