e5e901a1e5b43623f389366899778de8d3f23c46
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-10-27 07:35:40 -0300

    enemigos funcan en el bosque + UniqueKeyProcess en ThreadManager

--- Assets/Game/#Scenes/LoadByScene/ThreadProcess/ThreadObject.cs
+++ Assets/Game/#Scenes/LoadByScene/ThreadProcess/ThreadObject.cs
@@ -7,9 +7,11 @@ public class ThreadObject
     IEnumerator process;
     Action local_finish;
     string name_process = "process";
+    string keyUniqueProcess;
+    public string Key_Unique_Process { get { return keyUniqueProcess; } }
     public string Name_Process { get { return name_process; } }
 
-    public ThreadObject(IEnumerator _process, string _name = "process" , Action _local_finish = null)
+    public ThreadObject(IEnumerator _process, string _name = "process", string UniqueProcess = "default" , Action _local_finish = null)
     {
         process = _process;
         local_finish = _local_finish;
