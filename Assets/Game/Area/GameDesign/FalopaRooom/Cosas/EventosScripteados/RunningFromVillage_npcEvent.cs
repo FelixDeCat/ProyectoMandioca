1dd0c510e91edf3fc2622a688f426ce31403bae2
Frano <frano.criscuolo@gmail.com>
2020-10-31 18:58:01 -0300

    boludeces

--- Assets/Game/Area/GameDesign/FalopaRooom/Cosas/EventosScripteados/RunningFromVillage_npcEvent.cs
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/EventosScripteados/RunningFromVillage_npcEvent.cs
@@ -4,40 +4,51 @@ using UnityEngine;
 
 public class RunningFromVillage_npcEvent : Villager
 {
+    public Transform point;
+
     protected override void OnFixedUpdate()
     {
-        
+
     }
 
     protected override void OnInitialize()
     {
-       
+
     }
 
     protected override void OnPause()
     {
-     
+
     }
 
     protected override void OnResume()
     {
-     
+
     }
 
     protected override void OnTurnOff()
     {
-     
+
     }
 
     protected override void OnTurnOn()
     {
-     
+
     }
 
     protected override void OnUpdateEntity()
     {
-     
+
     }
 
- 
+    private void Start()
+    {
+        
+    }
+
+    
+
+    public void GoTo_Spot() { GoTo(point.position); Debug.Log("asdasdsa");}
+
+
 }
