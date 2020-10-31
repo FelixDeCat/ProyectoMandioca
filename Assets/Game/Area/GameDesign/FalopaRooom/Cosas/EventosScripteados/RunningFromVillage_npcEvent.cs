3be9051530d797a04a6aef3bc2221e6fc47dc4af
Frano <frano.criscuolo@gmail.com>
2020-10-31 18:12:57 -0300

    Camino base

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/EventosScripteados/RunningFromVillage_npcEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningFromVillage_npcEvent : Villager
+{
+    protected override void OnFixedUpdate()
+    {
+        
+    }
+
+    protected override void OnInitialize()
+    {
+       
+    }
+
+    protected override void OnPause()
+    {
+     
+    }
+
+    protected override void OnResume()
+    {
+     
+    }
+
+    protected override void OnTurnOff()
+    {
+     
+    }
+
+    protected override void OnTurnOn()
+    {
+     
+    }
+
+    protected override void OnUpdateEntity()
+    {
+     
+    }
+
+ 
+}
