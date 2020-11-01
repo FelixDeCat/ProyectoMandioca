bb116f862ef7be616d4a7c3c1c09ba5e23b54993
pablo Torre <pablo.torre@davinci.edu.ar>
2020-11-01 00:21:23 -0300

    Hay un spawner de jacintas para todo tipo de fantasias que quieran

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/Endless/NPCFleing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFleing : Villager
+{
+    public PointToGo pos_exit_endless;
+
+    protected override void OnInitialize() { }
+
+    public void GoToPos_ExitEndless() => GoTo(pos_exit_endless.transform.position);
+
+    //no se para que sirve esto, pero tira error sino, asique decora muy bien
+    #region En desuso
+    protected override void OnFixedUpdate() { }
+    protected override void OnPause() { }
+    protected override void OnResume() { }
+    protected override void OnTurnOff() { }
+    protected override void OnTurnOn() { }
+    protected override void OnUpdateEntity()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/Endless/NPCSpawnerEndless.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawnerEndless : MonoBehaviour
+{
+    [SerializeField] GameObject pelotudosASpawnear;
+    [SerializeField] Transform spawnPoint1;
+    [SerializeField] PointToGo endPoint;
+    [SerializeField] float cdSpawn;
+    [SerializeField] int taradosToSpawn;
+    [SerializeField] int taradosToWin;
+    int taradosSpawned;
+
+    public void SpawnTarados()
+    {
+        if (taradosSpawned < taradosToWin)
+        {
+            for (int i = 0; i < taradosToSpawn; i++)
+            {
+                taradosSpawned++;
+                GameObject Spawned = GameObject.Instantiate(pelotudosASpawnear);
+                Spawned.transform.position = spawnPoint1.transform.position;
+                Spawned.GetComponent<Jacinta>().Initialize();
+                Spawned.GetComponent<Jacinta>().pos_doctor = endPoint;
+                Spawned.GetComponent<Jacinta>().GoToPos_Doctor();
+            }
+            Invoke("SpawnTarados", cdSpawn);
+        }
+    }
+}
