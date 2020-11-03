cf24cd395bb4d264b3dc60dc90d58a1a43d3b0ff
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-02 22:47:06 -0300

    algunos fixes mas en la Dung

--- Assets/Game/Area/Scripts/World/Entities/Character/AnimEvent.cs
+++ Assets/Game/Area/Scripts/World/Entities/Character/AnimEvent.cs
@@ -6,8 +6,8 @@ public class AnimEvent : MonoBehaviour
 {
     EventManager myeventManager;
     private void Awake() => myeventManager = new EventManager();
-    public void Add_Callback(string s, Action receiver) => myeventManager.SubscribeToEvent(s, receiver);
-    public void Remove_Callback(string s, Action receiver) => myeventManager.UnsubscribeToEvent(s, receiver);
+    public void Add_Callback(string s, Action receiver) { if (myeventManager != null) myeventManager.SubscribeToEvent(s, receiver); else { Debug.Log("ME ESTAN QUERIENDO AGREGAR UN CALLBACK ANTES DE MI AWAKE"); } }
+    public void Remove_Callback(string s, Action receiver) { if (myeventManager != null) myeventManager.UnsubscribeToEvent(s, receiver); }
 
     //este es la funcion que vamos a disparar desde las animaciones
     public void EVENT_Callback(string s)  {myeventManager.TriggerEvent(s);} 
