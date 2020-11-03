16fbe11f9c1c4dc07a1cd01090b937f800d526df
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-03 20:14:01 -0300

    cuervo catcher eventos camino

---
+++ Assets/EventosCamino.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventosCamino : MonoBehaviour
+{
+    [SerializeField] ObjetiveSubscriber objective_kill_cuervo;
+
+    public void BeginEventosCaminoCuervo()
+    {
+        objective_kill_cuervo.BeginObjetive(OnKillCuervo);
+    }
+
+    void OnKillCuervo()
+    {
+        Debug.Log("KIL CUERVO");
+       
+    }
+}
