397daa7d327f15fcbfd29e9ca528111185ecc0b0
Frano <frano.criscuolo@gmail.com>
2020-11-03 23:07:49 -0300

    Cosas que no me quedaron bien

--- Assets/EventosCamino.cs
+++ Assets/EventosCamino.cs
@@ -6,14 +6,29 @@ public class EventosCamino : MonoBehaviour
 {
     [SerializeField] ObjetiveSubscriber objective_kill_cuervo;
 
+    [SerializeField] List<NPC_Anims> personitasEscondidas = new List<NPC_Anims>();
+
+    private void Start()
+    {
+        for (int i = 0; i < personitasEscondidas.Count; i++)
+        {
+            personitasEscondidas[i].GetComponent<RigidbodyPathFinder>().Initialize(personitasEscondidas[i].GetComponent<Rigidbody>());
+        }
+    }
+
     public void BeginEventosCaminoCuervo()
     {
+        Debug.Log("armo el evento");
         objective_kill_cuervo.BeginObjetive(OnKillCuervo);
     }
 
     void OnKillCuervo()
     {
-        Debug.Log("KIL CUERVO");
+        Debug.Log("corran mis chiquitos");
+        for (int i = 0; i < personitasEscondidas.Count; i++)
+        {
+            personitasEscondidas[i].GetComponent<NPCFleing>().GoToPos_RunningDesesperated();
+        }
        
     }
 }
