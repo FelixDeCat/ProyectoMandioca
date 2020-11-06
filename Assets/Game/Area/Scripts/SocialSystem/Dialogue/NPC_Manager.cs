19f4b14a3324cbc97caa06458eeeb384acd3444c
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-06 11:13:01 -0300

    algunos arreglos + contadorcito para agus

---
+++ Assets/Game/Area/Scripts/SocialSystem/Dialogue/NPC_Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class NPC_Manager : LoadComponent
+{
+    public string sceneName = "";
+    [SerializeField] NPCBase[] npcs = new NPCBase[0];
+
+    bool AlreadyProcessed = false;
+
+    [Header("ExecuteInEditMode")]
+    public bool ButtonRefresh = true;
+
+    protected void Start() // por si mi parent no viene con Loader
+    {
+        if (!AlreadyProcessed && Application.isPlaying)
+        {
+            AlreadyProcessed = true;
+            StartCoroutine(Process());
+        }
+    }
+
+    public void Test()
+    {
+        AlreadyProcessed = true;
+        StartCoroutine(Process());
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (ButtonRefresh)
+        {
+            ButtonRefresh = false;
+            npcs = GetComponentsInChildren<NPCBase>();
+        }
+    }
+#endif
+
+    protected override IEnumerator LoadMe()//por si mi parent me llamó a cargar
+    {
+        if (!AlreadyProcessed)
+        {
+            yield return Process();
+        }
+        else yield return null;
+    }
+
+    IEnumerator Process()
+    {
+        for (int i = 0; i < npcs.Length; i++)
+        {
+            npcs[i].Initialize();
+            yield return null;
+        }
+    }
+}
---
+++ Assets/GenCounter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GenCounter : UI_Base
+{
+    public TextMeshProUGUI info;
+    public TextMeshProUGUI cant;
+    public Animator animator;
+
+    public static GenCounter instance;
+    private void Awake() => instance = this;
+
+    public static void SetCounterInfo(string info_to_count, int current_val, int max_val, bool anim = false) => instance.SetInfo(info_to_count, current_val, max_val, anim);
+    void SetInfo(string info_to_count, int current_val, int max_val, bool anim = false)
+    {
+        info.text = info_to_count;
+        cant.text = current_val.ToString() + " / " + max_val.ToString();
+        if (anim)
+        {
+            animator.Play("CountAnim");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+
+            Open();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+            SetCounterInfo("test", 2, 10, true);
+        }
+    }
+
+    public override void Refresh() { }
+    protected override void OnAwake() { }
+    protected override void OnEndCloseAnimation() { }
+    protected override void OnEndOpenAnimation() { }
+    protected override void OnStart() { }
+    protected override void OnUpdate() { }
+}
