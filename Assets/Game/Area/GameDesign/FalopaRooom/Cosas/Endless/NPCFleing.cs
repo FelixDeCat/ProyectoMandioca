991a008b5615b53b38d8700fb11e74777cb7983a
pablo Torre <pablo.torre@davinci.edu.ar>
2020-11-06 07:59:02 -0300

    Muchas cosas de escenas varias y puestas las bases para todo lo del endless y pueblo, pero no funcan, so xd

--- Assets/Game/Area/GameDesign/FalopaRooom/Cosas/Endless/NPCFleing.cs
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/Endless/NPCFleing.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class NPCFleing : Villager
 {
     public PointToGo pos_exit_endless;
+    public PointToGo pos_exit_endless2;
+    public PointToGo pos_exit_endless3;
+    public PointToGo pos_exit_endless4;
 
     protected override void OnInitialize() { }
 
     public void GoToPos_ExitEndless() => GoTo(pos_exit_endless.transform.position); 
     public void GoToPos_RunningDesesperated() {GoToNoAnim(pos_exit_endless.transform.position, () => anim.StopRunDesesperate("")); anim.StartRunDesesperate(""); }
+    public void GoToPos_RunningDesesperatedSecondPoint() {GoToNoAnim(pos_exit_endless2.transform.position, () => anim.StopRunDesesperate("")); anim.StartRunDesesperate(""); }
+    public void GoToPos_RunningDesesperatedThirdPoint() {GoToNoAnim(pos_exit_endless3.transform.position, () => anim.StopRunDesesperate("")); anim.StartRunDesesperate(""); }
+    public void GoToPos_RunningDesesperatedFourthPoint() {GoToNoAnim(pos_exit_endless4.transform.position, () => anim.StopRunDesesperate("")); anim.StartRunDesesperate(""); }
     public void GoToPos_RunningDesesperated(Vector3 pos) {GoToNoAnim(pos, () => anim.StopRunDesesperate("")); anim.StartRunDesesperate(""); }
 
     public void AddMax()
