10a65764a9ea0a4aff41f2d335012720472df0d7
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2020-11-18 11:50:18 -0300

    todos los checkpoints con su refe a la escena que carga

---
+++ Assets/Game/Area/Scripts/ScriptsCore/GameLoop/CheckpointHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CheckpointHelper : MonoBehaviour
+{
+    public bool execute;
+    public LayerMask mask_hit_floor;
+
+    void Update()
+    {
+        if (execute)
+        {
+            execute = false;
+
+            var checkpoint = GetComponent<Checkpoint>();
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(this.transform.position + this.transform.up * 10, this.transform.up * -1, out hit, 30, mask_hit_floor))
+            {
+                checkpoint.sceneName = hit.collider.gameObject.scene.name;
+            }
+        }
+    }
+}
