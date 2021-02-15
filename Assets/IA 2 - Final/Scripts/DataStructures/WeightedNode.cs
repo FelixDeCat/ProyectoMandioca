5f7ebb80c717e1f14daae6b820516b540b87603e
SantiMinio <santiago.mino@davinci.edu.ar>
2021-02-15 16:54:06 -0300

    Trayendo Scripts

---
+++ Assets/IA 2 - Final/Scripts/DataStructures/WeightedNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace IA2Final
+{
+    [Serializable]
+    public class WeightedNode<T>
+    {
+
+        [SerializeField] private T _element;
+        [SerializeField] private float _weight;
+
+        public T Element => _element;
+        public float Weight => _weight;
+
+
+        public WeightedNode(T element, float weight)
+        {
+            _element = element;
+            _weight = weight;
+        }
+
+    }
+}
