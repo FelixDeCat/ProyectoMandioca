56709268ee82e0fa5c37937edfd383adfa3540bf
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2021-02-20 16:31:16 -0300

    llave y checker de enemigos

---
+++ Assets/IA 2 - Final/Scripts/Others/Enemy_Checker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Checker : MonoBehaviour
+{
+    [SerializeField] float _radius;
+    [SerializeField] LayerMask _enemyLayer;
+    [SerializeField] GameObject _key;
+    [SerializeField] Transform _pivot;
+    [SerializeField] float _updatetime;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Checking());
+    }
+    IEnumerator Checking()
+    {
+        yield return new WaitForSeconds(_updatetime);
+        var overlapsphere = Physics.OverlapSphere(transform.position, _radius,_enemyLayer);
+
+        if (overlapsphere.Length - 1 == 0)
+        {
+            var key=Instantiate(_key);
+            key.transform.position = _pivot.position;
+            Destroy(this.gameObject);
+        }
+       
+    }
+}
