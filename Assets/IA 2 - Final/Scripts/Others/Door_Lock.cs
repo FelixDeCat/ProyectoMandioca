dcf5d125c7fbdec58952e1ede9ad3b1bb09c9613
SantiMinio <santiago.mino@davinci.edu.ar>
2021-03-02 18:56:07 -0300

    Cheat para abrir la puerta

--- Assets/IA 2 - Final/Scripts/Others/Door_Lock.cs
+++ Assets/IA 2 - Final/Scripts/Others/Door_Lock.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DevelopTools.UI;
 
 public class Door_Lock : MonoBehaviour
 {
@@ -8,19 +9,31 @@ public class Door_Lock : MonoBehaviour
     [SerializeField] GameObject doorOpen;
     AudioSource As;
     bool opened;
+    private void Start()
+    {
+        Debug_UI_Tools.instance.CreateToogle("Abrir puerta del boss", false, OpenDoor);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Key_container>())
         {
-            if (opened == false)
-            {
-                doorClosed.SetActive(false);
-                doorOpen.SetActive(true);
-                As = GetComponent<AudioSource>();
-                As.Play();
-                opened = true;
-            }
+            OpenDoor(true);
            
         }
+    }
+
+    string OpenDoor(bool value)
+    {
+        if (opened == false)
+        {
+            doorClosed.SetActive(false);
+            doorOpen.SetActive(true);
+            As = GetComponent<AudioSource>();
+            As.Play();
+            opened = true;
+        }
+
+        return opened ? "Abierto" : "Cerrado";
     }
 }
