5a0ddab43a481ba59bd84b594c144a329bdb6c7d
Frano <frano.criscuolo@gmail.com>
2021-03-12 19:47:51 -0300

    RashoLazer

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/RashoLazer/LineRenderer_AnimatorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRenderer_AnimatorController : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    [SerializeField] Texture[] textures;
+    int animationStep;
+    [SerializeField] float fps = 30f;
+    float fpsCount = 0f;
+
+    public Texture current;
+
+    Material mat;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        fpsCount += Time.deltaTime;
+
+        if (fpsCount >= 1f / fps)
+        {
+            animationStep++;
+            if (animationStep == textures.Length)
+                animationStep = 0;
+
+            lineRenderer.material.SetTexture("_MainText", textures[animationStep]);
+
+            fpsCount = 0;
+        }
+
+
+        current = lineRenderer.material.GetTexture("_MainText");
+    }
+}
+
---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Cosas/RashoLazer/RayoLaser_Bounce.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RayoLaser_Bounce : MonoBehaviour
+{
+    [SerializeField] int reflections = 0;
+    [SerializeField] float maxLength = 100f;
+
+    LineRenderer lineRenderer;
+    Ray ray;
+    RaycastHit hit;
+    Vector3 direction;
+
+    bool on = true;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Restart()
+    {
+        on = true;
+        lineRenderer.enabled = on;
+    }
+
+    private void Update()
+    {
+        if (!on) return;
+
+        ray = new Ray(transform.position, transform.forward);
+
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+        float remainingLength = maxLength;
+
+        for (int i = 0; i < reflections; i++)
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
+            {
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
+                remainingLength -= Vector3.Distance(ray.origin, hit.point);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+
+                if (hit.collider.tag != "Mirror")
+                    break;
+            }
+            else
+            {
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+            }
+        }
+
+    }
+}
