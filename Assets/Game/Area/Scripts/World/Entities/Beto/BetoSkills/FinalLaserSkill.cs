e11df35adbe1ad4c91c5f93510115a286611b4c2
SantiMinio <santiago.mino@davinci.edu.ar>
2021-04-03 19:33:06 -0300

    Rasho laser :eyeglasses:

--- Assets/Game/Area/Scripts/World/Entities/Beto/BetoSkills/FinalLaserSkill.cs
+++ Assets/Game/Area/Scripts/World/Entities/Beto/BetoSkills/FinalLaserSkill.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class FinalLaserSkill : BossSkills
 {
+    [SerializeField] ThrowData throwData = null;
+    [SerializeField] Transform shootPos = null;
+
+    [SerializeField] Throwable projectile = null;
+    Transform target;
+
     public override void Initialize()
     {
         base.Initialize();
+        ThrowablePoolsManager.instance.CreateAPool(projectile.name, projectile);
+        SetTarget(Main.instance.GetChar().transform);
+    }
+
+    public BossSkills SetTarget(Transform _target)
+    {
+        target = _target;
+        return this;
     }
 
     protected override void OnUseSkill()
     {
+        ShootEvent();
     }
 
     protected override void OnInterruptSkill()
@@ -19,5 +34,13 @@ public class FinalLaserSkill : BossSkills
 
     protected override void OnOverSkill()
     {
+    }
+
+    void ShootEvent()
+    {
+        throwData.Position = shootPos.position;
+        throwData.Direction = (target.position + Vector3.up - shootPos.position).normalized;
+        ThrowablePoolsManager.instance.Throw(projectile.name, throwData);
+        Itteration(1, 2, delegate { }, OverSkill);
     }
 }
