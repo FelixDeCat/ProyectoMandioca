a0691b4bcd5ebc1cbe63537dc326ba26dbe5c63e
unknown <ramiro.cornejo@davinci.edu.ar>
2021-04-26 21:43:58 -0300

    Shader  de pasto reintegrado (le falta unas cosas asi que tranquilidad si ven cosas raras)

---
+++ Assets/Game/Area/Art/Shaders/Materials&Shaders/Effects/LowPoly/LowPolyReborn/Base/TestCorn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class TestCorn : MonoBehaviour
+{
+    public Material mat;
+    public Transform pos;
+
+    private void Update()
+    {
+       Shader.SetGlobalVector("PosPepito", pos.position);
+    }
+}
