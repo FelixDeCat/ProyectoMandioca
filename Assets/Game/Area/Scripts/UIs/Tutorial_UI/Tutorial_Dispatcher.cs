784a0eaece87cea2022cd51131c3223d4820f990
SantiMinio <santiago.mino@davinci.edu.ar>
2021-05-02 18:29:39 -0300

    La cosa de los interactables

--- Assets/Game/Area/Scripts/UIs/Tutorial_UI/Tutorial_Dispatcher.cs
+++ Assets/Game/Area/Scripts/UIs/Tutorial_UI/Tutorial_Dispatcher.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Tutorial_Dispatcher : MonoBehaviour
 {
     [SerializeField] TutorialSettings settings = null;
+    bool alreadyDelay;
 
     public void DispatchTutorial()
     {
-        if (settings == null) return;
+        if (settings == null || alreadyDelay) return;
+        Tutorial_UIController.instance.SetNewTutorial(settings);
+        enabled = false;
+        settings = null;
+    }
+
+    public void DispatchTutorialWithDelay(float delay)
+    {
+        if (settings == null || alreadyDelay) return;
+
+        alreadyDelay = true;
+        StartCoroutine(DelayTutorial(delay));
+    }
+
+    IEnumerator DelayTutorial(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         Tutorial_UIController.instance.SetNewTutorial(settings);
         enabled = false;
         settings = null;
