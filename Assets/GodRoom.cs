c97606ecae2fe9039fcc8ce4bec65a86e97eebac
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2021-05-04 06:01:54 -0300

    godroom para debug

---
+++ Assets/GodRoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodRoom : MonoBehaviour
+{
+    InteractableTeleport teleport;
+    private void Awake()
+    {
+        teleport = GetComponent<InteractableTeleport>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
+        {
+            teleport.Execute(Main.instance.GetChar());
+        }
+    }
+}
