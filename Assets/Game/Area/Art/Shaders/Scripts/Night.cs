aac1b7f0ef08fabf07259d6194532cc10bfeb9d9
unknown <ramiro.cornejo@davinci.edu.ar>
2021-05-08 19:58:34 -0300

    Cambios pp y transiciones

--- Assets/Game/Area/Art/Shaders/Scripts/Night.cs
+++ Assets/Game/Area/Art/Shaders/Scripts/Night.cs
@@ -11,27 +11,39 @@ public class Night : MonoBehaviour
 
     public Animator skyboxAnim;
 
+    public Material sky;
+
+    private void Awake()
+    {
+        sky.SetFloat("_Night", 0);
+
+    }
+
     public void SunTransition()
     {
+
+        sky.SetFloat("_Night", 1);
+
+
         _change = !_change;
 
         if (_change)
         {
             nightSun.enabled = true;
             sun.enabled = false;
-            skyboxAnim.SetBool("Start",_change);
-            skyboxAnim.SetBool("NewStart", true);
-
-
-        }
-        else
-        {
             skyboxAnim.SetBool("Start", _change);
-            nightSun.enabled = false;
-            sun.enabled = true;
             skyboxAnim.SetBool("NewStart", true);
-            
+
+
         }
+        //else
+        //{
+        //    skyboxAnim.SetBool("Start", _change);
+        //    nightSun.enabled = false;
+        //    sun.enabled = true;
+        //    skyboxAnim.SetBool("NewStart", true);
+
+        //}
     }
 
 }
