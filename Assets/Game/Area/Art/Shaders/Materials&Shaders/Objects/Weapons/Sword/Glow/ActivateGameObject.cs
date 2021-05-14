91c08f51895904abcf6ac4fad70ac06a301b3376
unknown <ramiro.cornejo@davinci.edu.ar>
2021-05-14 16:51:42 -0300

    Arreglo del heavy

---
+++ Assets/Game/Area/Art/Shaders/Materials&Shaders/Objects/Weapons/Sword/Glow/ActivateGameObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameObject : MonoBehaviour
+{
+    public ParticleSystem particle;
+    public ParticleSystem particleTwo;
+
+    public GameObject obj;
+
+    private void Update()
+    {
+        if (particle.isPlaying)
+            obj.SetActive(true);
+        else if(particleTwo.isStopped)
+            obj.SetActive(false);
+    }
+}
