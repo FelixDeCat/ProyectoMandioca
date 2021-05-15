6d972b8bbdda310c9a1ee705d0611b5837f03ef5
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2021-05-15 05:05:44 -0300

    puertitas con mas cariño + puerta en la jojodungeon

---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/Doors/ClassicDoor.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class ClassicDoor : DoorCore
+{
+    ////////////////////////////////////////////////////////////////////////
+    /// MUY IMPORTANTE !!!
+    /// El canto de la puerta tiene que estar 
+    /// apuntando al forward del Pivot
+    ////////////////////////////////////////////////////////////////////////
+
+    [Header("Classic Door Options")]
+    [SerializeField] Transform pivot;
+
+    [SerializeField] Transform look_at_to_open;
+    [SerializeField] Transform look_at_to_close;
+
+    Vector3 Forward_Open
+    {
+        get
+        {
+            var dir_open = look_at_to_open.transform.position - pivot.transform.position;
+            dir_open.y = 0;
+            return dir_open.normalized;
+        }
+    }
+    Vector3 Forward_Close
+    {
+        get
+        {
+            var dir_open = look_at_to_close.transform.position - pivot.transform.position;
+            dir_open.y = 0;
+            return dir_open.normalized;
+        }
+    }
+
+    [Header("Gizmos")]
+    [SerializeField] bool draw_forward = true;
+    [SerializeField] bool draw_up = true;
+    [SerializeField] bool draw_Open_And_Close = true;
+
+    private void Start()
+    {
+        pivot.forward = Forward_Close;
+    }
+
+    #region Condicion, si no tiene ninguna en especial, devolver true
+    protected override bool CustomCondition() { return true; }
+    #endregion
+
+    #region Animacion
+    protected override void LerpValue(float lerp_value)
+    {
+        pivot.forward = Vector3.Lerp(Forward_Close, Forward_Open, lerp_value);
+    }
+    #endregion
+
+    #region FEEDBACKS
+    [Header("UNITY EVENTS FEEDBACKS")]
+    [SerializeField] ClassicDoorFeedbacks feedback;
+    protected override void Feedback_On_Begin_Open() { feedback.Feedback_On_Begin_Open.Invoke(); }
+    protected override void Feedback_On_End_Open() { feedback.Feedback_On_End_Open.Invoke(); }
+    protected override void Feedback_On_Begin_Close() { feedback.Feedback_On_Begin_Close.Invoke(); }
+    protected override void Feedback_On_End_Close() { feedback.Feedback_On_End_Close.Invoke(); }
+    protected override void Feedback_On_Execution_Failed() { feedback.Feedback_On_Execution_Failed.Invoke(); }
+    #endregion
+
+    int DIST = 20;
+    private void OnDrawGizmos()
+    {
+        if (pivot)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (draw_forward)
+                {
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawRay(pivot.position + (Vector3.up * i / 2), pivot.forward * 10);
+                }
+                if (draw_Open_And_Close)
+                {
+                    if (look_at_to_open)
+                    {
+                        var dir_open = look_at_to_open.transform.position - pivot.transform.position;
+                        dir_open.y = 0;
+                        Gizmos.color = Color.white;
+                        Gizmos.DrawRay(pivot.position + (Vector3.up * i / 2), dir_open.normalized * 10);
+                    }
+                    if (look_at_to_close)
+                    {
+                        var dir_close = look_at_to_close.transform.position - pivot.transform.position;
+                        dir_close.y = 0;
+                        Gizmos.color = Color.black;
+                        Gizmos.DrawRay(pivot.position + (Vector3.up * i / 2), dir_close.normalized * 10);
+                    }
+                }
+            }
+
+            if (draw_up)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(pivot.position, pivot.up * DIST);
+            }
+        }
+    }
+
+    [System.Serializable]
+    internal class ClassicDoorFeedbacks
+    {
+        [SerializeField] internal UnityEvent
+        Feedback_On_Begin_Open,
+        Feedback_On_End_Open,
+        Feedback_On_Begin_Close,
+        Feedback_On_End_Close,
+        Feedback_On_Execution_Failed;
+    }
+}
+
+
---
+++ Assets/Game/Area/GameDesign/FalopaRooom/Scripts/Doors/DoorCore.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DoorCore : MonoBehaviour
+{
+    bool open = false;
+    bool isAnim = false;
+    bool animate_open = false;
+    float timer = 0;
+    [Header("Core Door Options")]
+    [SerializeField] float time_to_open = 2.0f;
+    [SerializeField] float time_to_close = 2.0f;
+
+    enum animtype { Classic_Door, Other };
+    [SerializeField] animtype animation_type = animtype.Classic_Door;
+
+    /// <summary> INVIERTE (si me ejecutaron dos veces lo hace otra vez pero al revez) </summary>
+    public void Switch()
+    {
+        if (!isAnim && CustomCondition())
+        {
+            animate_open = !open;
+            isAnim = true;
+            timer = 0;
+        }
+        else
+        {
+            Feedback_On_Execution_Failed();
+        }
+    }
+
+    /// <summary> SOLO ABRE (si me ejecutaron dos veces no hace nada) </summary>
+    public void Open()
+    {
+        if (!isAnim && !open && CustomCondition())
+        {
+            animate_open = true;
+            isAnim = true;
+            timer = 0;
+        }
+        else
+        {
+            Feedback_On_Execution_Failed();
+        }
+    }
+
+    /// <summary> SOLO CIERRA (si me ejecutaron dos veces no hace nada) </summary>
+    public void Close()
+    {
+        if (!isAnim && open && CustomCondition())
+        {
+            animate_open = false;
+            isAnim = true;
+            timer = 0;
+        }
+        else
+        {
+            Feedback_On_Execution_Failed();
+        }
+    }
+
+    protected abstract bool CustomCondition();
+    protected abstract void LerpValue(float lerp_value);
+
+    /// <summary> se ejecuta cuando empieza a abrirse </summary>
+    protected abstract void Feedback_On_Begin_Open();
+    protected abstract void Feedback_On_End_Open();
+    protected abstract void Feedback_On_Begin_Close();
+    protected abstract void Feedback_On_End_Close();
+    protected abstract void Feedback_On_Execution_Failed();
+
+    void Animate(float val) => LerpValue(val);
+
+    private void Update()
+    {
+        if (!isAnim) return;
+
+        switch (animation_type)
+        {
+            case animtype.Classic_Door:
+
+                float MAX = animate_open ? time_to_open : time_to_close; // ¿ que máximo voy a usar ?
+
+                if (timer < MAX)
+                {
+                    timer = timer + 1 * Time.deltaTime;
+
+                    float normalized_direction = 
+                        animate_open ?          // ¿ estoy abriendo ?
+                        timer / MAX :           // voy de 0 hasta 1
+                        1 - (timer / MAX);      // voy de 1 hasta 0
+
+                    Animate(normalized_direction);
+                }
+                else
+                {
+                    open = animate_open;                // lo seteo abierto o cerrado dependiendo de la animacion que me ejecutaron
+                    Animate(animate_open ? 1 : 0);      // clamp por si las moscas
+                    timer = 0;
+                    isAnim = false;
+                }
+                
+                break;
+
+            case animtype.Other:
+
+                //si quieren romper este código haganlo acá, ¡no me toquen la puerta clásica! >:c
+
+                break;
+        }
+    }
+}
