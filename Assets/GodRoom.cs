fa3b3efaa9c1ca46c1cff94191a04e210bb1f7eb
Guillermo Felix Romero <guillermo.romero@davinci.edu.ar>
2021-05-17 21:11:10 -0300

    D1 lo contiene todo , Adios a los Level Details

--- Assets/GodRoom.cs
+++ Assets/GodRoom.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GodRoom : MonoBehaviour
 {
     InteractableTeleport teleport;
+
+    public Transform transform_destino;
+
+    public UnityEvent EnterGodRoom;
+    public UnityEvent ExitGodRoom;
     private void Awake()
     {
-        teleport = GetComponent<InteractableTeleport>();
+        //teleport = GetComponent<InteractableTeleport>();
     }
 
     // Update is called once per frame
@@ -15,7 +21,22 @@ public class GodRoom : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.G) && Input.GetKeyDown(KeyCode.R))
         {
-            teleport.Execute(Main.instance.GetChar());
+            if (Input.GetKey(KeyCode.G) && Input.GetKeyDown(KeyCode.R))
+            {
+                Main.instance.GetChar().GetCharMove().StopDamageFall();
+                Main.instance.GetChar().transform.position = transform_destino.position;
+                OnEnterGodRoom();
+            }
+
         }
+    }
+
+    public void UE_OnExitGodRoom()
+    {
+        ExitGodRoom.Invoke();
+    }
+    public void OnEnterGodRoom()
+    {
+        EnterGodRoom.Invoke();
     }
 }
