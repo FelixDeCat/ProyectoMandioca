3694783829a288bb99c9a7f11c31a5c7281ab11b
SantiMinio <santiago.mino@davinci.edu.ar>
2021-05-20 21:34:31 -0300

    Entusiasta en la granja

---
+++ Assets/FastScripts/AssassinRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AssassinRay : MonoBehaviour
+{
+    [SerializeField] ParticleSystem rayParticle = null;
+    [SerializeField] AudioClip groundHit = null;
+    [SerializeField] AudioClip lightningStrike = null;
+
+    private void Start()
+    {
+        ParticlesManager.Instance.GetParticlePool(rayParticle.name, rayParticle);
+        AudioManager.instance.GetSoundPool(groundHit.name, AudioManager.SoundDimesion.TwoD, AudioGroups.GAME_FX, groundHit);
+        AudioManager.instance.GetSoundPool(lightningStrike.name, AudioManager.SoundDimesion.TwoD, AudioGroups.GAME_FX, lightningStrike);
+    }
+
+    public void Attack()
+    {
+        ParticlesManager.Instance.PlayParticle(rayParticle.name, transform.position);
+        AudioManager.instance.PlaySound(lightningStrike.name, transform);
+        AudioManager.instance.PlaySound(groundHit.name, transform);
+        var overlap = Physics.OverlapSphere(transform.position, 15).Where(x => x.GetComponent<RagdollComponent>()).Where(x => !x.GetComponent<EnemyBase>())
+            .Select(x => x.GetComponent<RagdollComponent>());
+
+        foreach (var item in overlap)
+        {
+            item.ActivateRagdoll((item.transform.position - transform.position), () => Destroy(item.gameObject));
+        }
+    }
+}
---
+++ Assets/FastScripts/EntusiastaVillager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EntusiastaVillager : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints = new Transform[0];
+    [SerializeField] Animator anim = null;
+    [SerializeField] AssassinRay ray = null;
+    [SerializeField] UnityEvent[] reachToWaypoint = new UnityEvent[0];
+    [SerializeField] UnityEvent endThunderAnim = null;
+
+    [SerializeField] float speed = 5;
+
+    int currentWaypoint;
+    int targetWaypoint;
+    bool move;
+
+    private void Start()
+    {
+        anim.GetBehaviour<ANIM_SCRIPT_Base>().ConfigureCallback(() => endThunderAnim.Invoke());
+        anim.GetComponent<AnimEvent>().Add_Callback("SpawnOrb", ray.Attack);
+    }
+
+    public void GoToWaypoint(int waypoint)
+    {
+        targetWaypoint = waypoint;
+        move = true;
+        anim.SetBool("Move", true);
+    }
+
+    public void UpdateInteractable() => Main.instance.eventManager.TriggerEvent(GameEvents.INTERACTABLES_INITIALIZE);
+
+    private void Update()
+    {
+        if (move)
+        {
+            Vector3 dirToWaypoint = (waypoints[currentWaypoint + 1].position - transform.position).normalized;
+            transform.forward = dirToWaypoint;
+
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, waypoints[currentWaypoint + 1].position) <= 0.2f)
+            {
+                currentWaypoint += 1;
+                if(currentWaypoint == targetWaypoint)
+                {
+                    move = false;
+                    anim.SetBool("Move", false);
+                    reachToWaypoint[currentWaypoint].Invoke();
+                    transform.forward = waypoints[currentWaypoint].forward;
+                }
+            }
+        }
+    }
+}
