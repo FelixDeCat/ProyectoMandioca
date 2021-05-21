a827e9be90824dec4d77aa6e5edf4b783129185e
SantiMinio <santiago.mino@davinci.edu.ar>
2021-05-20 22:27:10 -0300

    Camara en entusiasta, portal debería desaparecer piedra

--- Assets/Game/Area/Scripts/ScriptsCore/Portal.cs
+++ Assets/Game/Area/Scripts/ScriptsCore/Portal.cs
@@ -4,25 +4,18 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    //[SerializeField] Portal conection = null;
+    [SerializeField] Portal conection = null;
     [SerializeField] RenderTexture portalRender = null;
     [SerializeField] MeshRenderer myMesh = null;
-    //[SerializeField] LayerMask transportable = 1 << 8;
-    // float cd;
-    // bool canTP;
+    [SerializeField] LayerMask transportable = 1 << 8;
+    float cd;
+    bool canTP;
 
     private void Start()
     {
         myMesh.material.SetTexture("_PortalRender", portalRender);
     }
-    /*
-
-    protected override void OnInitialize()
-    {
-        
-    }
-
-    protected override void OnUpdate()
+    void Update()
     {
         if (canTP)
         {
@@ -36,14 +29,12 @@ public class Portal : MonoBehaviour
         }
     }
 
-    protected override void OnFixedUpdate()
-    {
-    }
-
     void TPToPortal(Transform tpObject, Vector3 diff)
     {
         canTP = true;
         tpObject.position = myMesh.transform.position + diff;
+
+
         if (tpObject.GetComponent<CharacterHead>())
             tpObject.GetComponent<CharacterHead>().Root.forward = new Vector3(myMesh.transform.up.x, 0, myMesh.transform.up.z);
         else if (tpObject.GetComponent<BashingRock>())
@@ -55,24 +46,12 @@ public class Portal : MonoBehaviour
         if (canTP) return;
         if ((1 << other.gameObject.layer & transportable) != 0)
         {
+            if(conection == null)
+            {
+                if (other.GetComponent<BashingRock>()) other.transform.position = Vector3.zero;
+                return;
+            }
             conection.TPToPortal(other.transform, other.transform.position - myMesh.transform.position);
         }
     }
-
-    protected override void OnPause()
-    {
-    }
-
-    protected override void OnResume()
-    {
-    }
-
-    protected override void OnTurnOn()
-    {
-    }
-
-    protected override void OnTurnOff()
-    {
-    }
-    */
 }
