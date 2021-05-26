c7da0b981d8a43b2d14ddb0773cf91b754a78861
SantiMinio <santiago.mino@davinci.edu.ar>
2021-05-26 18:14:39 -0300

    Caronte arreglado (agora sim)

--- Assets/IA 2 - Final/Scripts/BossScripts/SpawnSkill.cs
+++ Assets/IA 2 - Final/Scripts/BossScripts/SpawnSkill.cs
@@ -38,6 +38,7 @@ public class SpawnSkill : BossSkills, ISpawner
 
     void SpawnEnemies()
     {
+        spot.spawnSpot.position = Main.instance.GetChar().transform.position + Vector3.up;
         int ammountToSpawn = UnityEngine.Random.Range(minSpawn, maxSpawn + 1);
         for (int i = 0; i < ammountToSpawn; i++)
         {
