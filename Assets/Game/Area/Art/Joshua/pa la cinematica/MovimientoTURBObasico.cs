dfedb9dffe01dc632815c2ad3ff334483d58f5bb
jojoshua-ohno <joshua.aguilarlynch@davinci.edu.ar>
2021-05-29 20:14:51 -0300

    cosas para la intro

---
+++ Assets/Game/Area/Art/Joshua/pa la cinematica/MovimientoTURBObasico.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoTURBObasico : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * 1.1f);
+    }
+}
