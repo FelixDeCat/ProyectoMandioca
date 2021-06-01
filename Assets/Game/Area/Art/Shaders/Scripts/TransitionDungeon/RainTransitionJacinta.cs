8bb21c4efc545b4354b2b1dad58dceae6043b09e
unknown <ramiro.cornejo@davinci.edu.ar>
2021-06-01 19:48:52 -0300

    Transiciones de pp y cosas

---
+++ Assets/Game/Area/Art/Shaders/Scripts/TransitionDungeon/RainTransitionJacinta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainTransitionJacinta : Switcheable
+{
+    public GameObject objTransition;
+
+    public override void ABSOnFade(float f)
+    {
+       
+    }
+
+    public override void ABSOnTurnOff()
+    {
+        objTransition.SetActive(false);
+    }
+
+    public override void ABSOnTurnON()
+    {
+        objTransition.SetActive(true);
+
+    }
+
+    
+}
