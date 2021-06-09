f1fb9185a2c4524f01c994d0cfbfcd7d1e8d6e7b
SantiMinio <santiago.mino@davinci.edu.ar>
2021-06-09 16:21:02 -0300

    """""""Modo foto"""""""""

---
+++ Assets/FastScripts/CameraMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMode : MonoBehaviour
+{
+    [SerializeField] Camera[] UICameras = new Camera[0];
+    bool dissapeared;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.J))
+            DissappearUI();
+    }
+
+    void DissappearUI()
+    {
+        Debug.Log("Hola?");
+        for (int i = 0; i < UICameras.Length; i++)
+        {
+            UICameras[i].enabled = dissapeared;
+        }
+        dissapeared = !dissapeared;
+    }
+}
