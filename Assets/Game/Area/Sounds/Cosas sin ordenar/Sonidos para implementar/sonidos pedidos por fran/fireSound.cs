964ca18f5ad8502bb1058b5a8139acd61db3ff2d
Tobilona108 <tobias.lonardi@davinci.edu.ar>
2021-06-09 22:59:09 -0300

    sonidos en los portales y fuegos

---
+++ Assets/Game/Area/Sounds/Cosas sin ordenar/Sonidos para implementar/sonidos pedidos por fran/fireSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireSound : MonoBehaviour
+{
+    [SerializeField] AudioClip _soundEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.GetSoundPool(_soundEffect.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, _soundEffect, true);
+        AudioManager.instance.PlaySound(_soundEffect.name,transform);
+        Debug.Log("ndea");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
