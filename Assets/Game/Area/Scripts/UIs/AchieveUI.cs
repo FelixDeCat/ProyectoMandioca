d569df225742bf622a671b07b78a044d599d9d89
SantiMinio <santiago.mino@davinci.edu.ar>
2021-07-07 04:38:56 -0300

    Un comienzo para la ui de logros

---
+++ Assets/Game/Area/Scripts/UIs/AchieveUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AchieveUI : MonoBehaviour
+{
+    [SerializeField] Image achieveImg = null;
+    [SerializeField] Image background = null;
+    [SerializeField] TextMeshProUGUI achieveTitle = null;
+    [SerializeField] TextMeshProUGUI achieveDesc = null;
+    [SerializeField] Color blockColor;
+    [SerializeField] Color completeColor;
+
+    public void SetAchieve(Achieves achieve, bool isComplete)
+    {
+        if (isComplete)
+        {
+            achieveImg.sprite = achieve.achiveImg;
+            achieveTitle.text = achieve.title;
+            achieveDesc.text = achieve.description;
+            background.color = completeColor;
+        }
+        else
+        {      
+            achieveTitle.text = "???";
+            achieveDesc.text = achieve.blockDescription;
+            background.color = blockColor;
+        }
+    }
+}
