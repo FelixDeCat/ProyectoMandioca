f9d429b119740e620f5b89995150e6916ab8872a
SantiMinio <santiago.mino@davinci.edu.ar>
2021-07-09 19:24:49 -0300

    Animaciones

--- Assets/Game/Area/Scripts/UIs/AchieveUI.cs
+++ Assets/Game/Area/Scripts/UIs/AchieveUI.cs
@@ -13,7 +13,19 @@ public class AchieveUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI achieveDesc = null;
     [SerializeField] Color blockColor;
     [SerializeField] Color completeColor;
+    [SerializeField] Vector3 scaleToSelect;
+    [SerializeField] Vector3 scaleToUnselect;
+    Vector3 targetScale;
+    bool isAnim;
+    [SerializeField]float animSpeed = 0.8f;
+    Vector3 scale;
+    RectTransform rect;
+    float timer;
 
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
     public void SetAchieve(Achieves achieve, bool isComplete)
     {
         if (isComplete)
@@ -34,11 +46,27 @@ public class AchieveUI : MonoBehaviour
 
     public void SelectAchieve()
     {
-
+        scale = rect.localScale;
+        isAnim = true;
+        targetScale = scaleToSelect;
+        timer = 0;
     }
 
     public void UnselectAchieve()
     {
+        scale = rect.localScale;
+        isAnim = true;
+        targetScale = scaleToUnselect;
+        timer = 0;
+    }
 
+    private void Update()
+    {
+        if (isAnim)
+        {
+            rect.localScale = Vector3.Lerp(scale, targetScale, timer += animSpeed * Time.deltaTime);
+            if (rect.localScale == targetScale)
+                isAnim = false;
+        }
     }
 }
