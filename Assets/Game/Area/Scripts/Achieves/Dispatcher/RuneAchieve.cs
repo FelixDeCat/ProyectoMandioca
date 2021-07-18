ea41188f4d4587d5c20b26d284f7ccd30d80b58e
SantiMinio <santiago.mino@davinci.edu.ar>
2021-07-18 18:24:03 -0300

    Teórico último logro, pero quién sabe

---
+++ Assets/Game/Area/Scripts/Achieves/Dispatcher/RuneAchieve.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneAchieve : MonoBehaviour
+{
+    [SerializeField] string achieveID = "Rune";
+    bool isActive = false;
+    [SerializeField] LayerMask obstacleMask;
+    [SerializeField] LineOfSight sight = null;
+
+    private void Awake()
+    {
+        sight.Configurate(Main.instance.GetChar().Root);
+    }
+
+    private void OnWillRenderObject()
+    {
+        if (!isActive)
+        {
+            if (sight.OnSight(transform))
+            {
+                AchievesManager.instance.CompleteAchieve(achieveID);
+                isActive = true;
+                Camera cac = Camera.main;
+            }
+        }
+    }
+}
