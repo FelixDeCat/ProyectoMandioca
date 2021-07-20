1f49397d1a2c79d0a7dca1e4681d20472f625252
SantiMinio <santiago.mino@davinci.edu.ar>
2021-07-20 16:55:02 -0300

    El comienzo de la cinematic del comienzo

---
+++ Assets/Game/Area/Scripts/CinematicScripts/MainCameraCinematic.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MainCameraCinematic : MonoBehaviour
+{
+    [SerializeField] WaypointCamera[] waypoints = new WaypointCamera[0];
+    [SerializeField] UnityEvent OnStartCinematic = new UnityEvent();
+    [SerializeField] UnityEvent OnEndCinematic = new UnityEvent();
+    [SerializeField] float startSpeed = 2;
+
+    bool shaking;
+    bool moving;
+    float shakeAmmount;
+    Vector3 currentCamPos;
+    Quaternion currentRotation;
+    int nextIndex = 0;
+    WaypointCamera current;
+    float currentSpeed;
+    float posLerp;
+    float timer;
+
+    public void StartCinematic()
+    {
+        transform.position = Main.instance.GetMyCamera().transform.position;
+        transform.rotation = Main.instance.GetMyCamera().transform.rotation;
+        Main.instance.GetMyCamera().GetComponent<Camera>().enabled = false;
+        GetComponent<Camera>().enabled = true;
+        moving = true;
+        currentRotation = transform.rotation;
+        currentCamPos = transform.position;
+        currentSpeed = startSpeed;
+    }
+
+    void EndCinematic()
+    {
+        Debug.Log("Se terminó lo que se daba");
+    }
+
+
+    private void Update()
+    {
+        if (moving)
+        {
+            posLerp += Time.deltaTime * currentSpeed;
+            transform.position = Vector3.Lerp(currentCamPos, waypoints[nextIndex].transform.position, posLerp);
+            transform.rotation = Quaternion.Lerp(currentRotation, waypoints[nextIndex].transform.rotation, posLerp);
+
+            if(posLerp >= 1)
+            {
+                current = waypoints[nextIndex];
+                nextIndex += 1;
+                currentCamPos = current.transform.position;
+                currentRotation = current.transform.rotation;
+                currentSpeed = current.changeSpeed;
+                current.EnterWaypoint();
+                posLerp = 0;
+                moving = false;
+            }
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= current.durationInWaypoint)
+            {
+                if(nextIndex >= waypoints.Length)
+                {
+                    EndCinematic();
+                    return;
+                }
+                moving = true;
+                timer = 0;
+                current.ExitWaypoint();
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (shaking)
+        {
+            transform.position += UnityEngine.Random.insideUnitSphere * shakeAmmount;
+        }
+    }
+}
---
+++ Assets/Game/Area/Scripts/CinematicScripts/WaypointCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaypointCamera : MonoBehaviour
+{
+    public float durationInWaypoint = 2;
+    public float changeSpeed = 4;
+    [SerializeField] UnityEvent OnEnterWaypoint = new UnityEvent();
+    [SerializeField] UnityEvent OnExitWaypoint = new UnityEvent();
+
+    public void EnterWaypoint() => OnEnterWaypoint.Invoke();
+
+    public void ExitWaypoint() => OnExitWaypoint.Invoke();
+}
