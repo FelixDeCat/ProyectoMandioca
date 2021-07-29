1bec7b686e2056c0e1bccbb3a1f535b98e7ecd11
jojoshua-ohno <joshua.aguilarlynch@davinci.edu.ar>
2021-07-28 22:14:16 -0300

    seteando la outrera

---
+++ Assets/Game/Area/Art/Joshua/OUTROSIIIII/MelemacoMovetePorFavor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelemacoMovetePorFavor : MonoBehaviour
+{
+    public Transform[] positions;
+    public GameObject ObjectToMove;
+    public float MoveSpeed = 8;
+    Coroutine MoveIE;
+
+    void Start()
+    {
+        StartCoroutine(moveObject());
+    }
+
+    IEnumerator moveObject()
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            MoveIE = StartCoroutine(Moving(i));
+            yield return MoveIE;
+        }
+    }
+
+    IEnumerator Moving(int currentPosition)
+    {
+        while (ObjectToMove.transform != positions[currentPosition])
+        {
+            ObjectToMove.transform.position = Vector3.MoveTowards(ObjectToMove.transform.position, new Vector3(positions[currentPosition].position.x, positions[currentPosition].position.y, positions[currentPosition].position.z), MoveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
