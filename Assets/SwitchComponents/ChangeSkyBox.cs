d573860ca6658c967abbbe0baf6198b632bc23c5
unknown <ramiro.cornejo@davinci.edu.ar>
2021-07-31 16:51:06 -0300

    Una cosa del skybox que no funciona : )

---
+++ Assets/SwitchComponents/ChangeSkyBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChangeSkyBox : Switcheable
+{
+    public PostProcessVolume pp;
+
+    public Material mat;
+
+    public Color groundColor;
+    public Color skyColor;
+
+    public Color groundColor_2;
+    public Color skyColor_2;
+
+    public override void ABSOnFade(float f)
+    {
+        
+    }
+
+    public override void ABSOnTurnOff()
+    {
+        mat.SetColor("_GroundColor", groundColor_2);
+        mat.SetColor("_SkyColor", skyColor_2);
+    }
+
+    public override void ABSOnTurnON()
+    {
+        mat.SetColor("_GroundColor", groundColor);
+        mat.SetColor("_SkyColor", skyColor);
+    }
+}
