f474ee142fbf414074c30eaf28e4af914a159e9b
SantiMinio <santiago.mino@davinci.edu.ar>
2021-08-03 18:48:55 -0300

    Más cosas de la cinemática para terminarla mañana

--- Assets/FastScripts/BetoCinematicSounds.cs
+++ Assets/FastScripts/BetoCinematicSounds.cs
@@ -4,15 +4,110 @@ using UnityEngine;
 
 public class BetoCinematicSounds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Start")]
+    [SerializeField] ParticleSystem flyParticles = null;
+    [SerializeField] Animator[] fonts = new Animator[0];
+    [SerializeField] Renderer mainFont = null;
+    [SerializeField] float timeToCorruptWater = 2;
+    [SerializeField] AudioClip statueOn = null;
+    [SerializeField] AudioClip screamSound = null;
+    [SerializeField] AudioClip earthquakeSound = null;
+    [SerializeField] string bossName = "Deimos";
+    [SerializeField] string bossDesc = "Dios Corrupto";
+
+
+    [Header("Dead")]
+    [SerializeField] AudioClip tentaclesSound = null;
+    [SerializeField] AudioClip colorOffSound = null;
+
+    private void Start()
     {
-        
+        AudioManager.instance.GetSoundPool(statueOn.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, statueOn);
+        AudioManager.instance.GetSoundPool(tentaclesSound.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, tentaclesSound);
+        AudioManager.instance.GetSoundPool(colorOffSound.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, colorOffSound);
+        AudioManager.instance.GetSoundPool(screamSound.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, screamSound);
+        AudioManager.instance.GetSoundPool(earthquakeSound.name, AudioManager.SoundDimesion.ThreeD, AudioGroups.GAME_FX, earthquakeSound);
+        //transform.localPosition = Vector3.up * 2000;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartCinematic()
     {
-        
+        transform.localPosition = Vector3.zero;
+        GetComponent<Animator>().Play("StartCinematic");
+        Debug.Log("???!?!");
     }
+
+    void END_TentacleSound()
+    {
+        AudioManager.instance.PlaySound(tentaclesSound.name, transform);
+    }
+
+    void END_ColorOff()
+    {
+        AudioManager.instance.PlaySound(colorOffSound.name, transform);
+    }
+
+    void END_ColorOffStop()
+    {
+        AudioManager.instance.StopAllSounds(colorOffSound.name);
+    }
+
+    void START_StatueOn()
+    {
+        AudioManager.instance.PlaySound(statueOn.name, transform);
+    }
+
+    void START_StopFlyParticle()
+    {
+        flyParticles.Stop();
+    }
+
+    public void START_CorruptFonts()
+    {
+        StartCoroutine(CorruptFonts());
+    }
+
+    IEnumerator CorruptFonts()
+    {
+        for (int i = 0; i < fonts.Length; i++)
+        {
+            fonts[i].Play("Llego la kk");
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void START_CorruptMainFont()
+    {
+        StartCoroutine(CorruptMainFont());
+    }
+
+    IEnumerator CorruptMainFont()
+    {
+        float timer = timeToCorruptWater;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            mainFont.material.SetFloat("_MainOpacity", Mathf.Lerp(0, 1, timer / timeToCorruptWater));
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void START_ScreamSound()
+    {
+        AudioManager.instance.PlaySound(screamSound.name, transform);
+    }
+
+    public void START_EarthquakeSound()
+    {
+        AudioManager.instance.PlaySound(earthquakeSound.name, transform.parent);
+    }
+    public void START_StopEarthquakeSound()
+    {
+        AudioManager.instance.StopAllSounds(earthquakeSound.name);
+    }
+
+
+    void ApppearBossName() => Main.instance.gameUiController.OpenBossName(bossName, bossDesc);
 }
